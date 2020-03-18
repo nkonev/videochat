3e02dfe97791de117e268e89b8351ed416f5ab8d
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-18 23:35:02 +0300

    comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+/**
+ * Main landing page user starts interaction from
+ * @returns {*}
+ * @constructor
+ */
 function App() {
     return (
         <div className="App">
