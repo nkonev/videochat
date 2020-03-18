853b9f49350b64723e02130e438d60236f38d3be
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-18 23:33:36 +0300

    initial navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,12 @@ import React from "react";
 function App() {
     return (
         <div className="App">
-            Landing here =)
+            <ul>
+                {/* Navbar */}
+                <li>Pricing</li>
+                <li>Login</li>
+                <li>My chats</li>
+            </ul>
         </div>
     )
 }
