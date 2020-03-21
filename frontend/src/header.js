5e5f76441601c033f75648372ec12c7139107b45
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-21 13:32:54 +0300

    rm header.js

--- frontend/src/header.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function showHeader() {
-    var header = document.querySelector('.header');
-    if(window.pageYOffset > 200){
-        header.classList.add('header_fixed');
-    } else{
-        header.classList.remove('header_fixed');
-    }
-}
