cbb1340d8d9d8771d8d4bfe4c11f2fd39ae945a6
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-24 03:13:39 +0300

    login with redirect back

--- gateway/src/main/java/name/nkonev/spring/cloud/gateway/SecurityConfig.java
+++ gateway/src/main/java/name/nkonev/spring/cloud/gateway/SecurityConfig.java
@@ -1,12 +1,18 @@
 package name.nkonev.spring.cloud.gateway;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,6 +21,7 @@ import org.springframework.security.core.userdetails.MapReactiveUserDetailsServi
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.security.web.server.ServerRedirectStrategy;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
@@ -23,7 +30,9 @@ import org.springframework.security.web.server.savedrequest.ServerRequestCache;
 import org.springframework.security.web.server.savedrequest.WebSessionServerRequestCache;
 import org.springframework.security.web.server.ui.LoginPageGeneratingWebFilter;
 import org.springframework.security.web.server.ui.LogoutPageGeneratingWebFilter;
+import org.springframework.security.web.server.util.matcher.*;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+import org.springframework.util.Assert;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
@@ -35,6 +44,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 
 @EnableRedisWebSession
 @EnableWebFluxSecurity
@@ -46,14 +56,22 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         LoginPageGeneratingWebFilter loginPageGeneratingWebFilter = new LoginPageGeneratingWebFilter();
         loginPageGeneratingWebFilter.setFormLoginEnabled(true);
+
+        ServerWebExchangeMatcher get = ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/**");
+        WebSessionServerRequestCache webSessionServerRequestCache = new WebSessionServerRequestCache();
+        webSessionServerRequestCache.setSaveRequestMatcher(get);
+
+        RedirectServerAuthenticationSuccessHandler authenticationSuccessHandler = new RedirectServerAuthenticationSuccessHandler();
+        authenticationSuccessHandler.setRequestCache(webSessionServerRequestCache);
+        authenticationSuccessHandler.setRedirectStrategy(new MyServerRedirectStrategy());
         return http.authorizeExchange()
                 .pathMatchers("/", "/public/**"/*, "/login", "/logout"*/).permitAll()
                 .pathMatchers("/api/**", "/self/**").authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
-//                .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/"))
+                .authenticationEntryPoint(new MyEntryPoint(HttpStatus.UNAUTHORIZED, webSessionServerRequestCache))
+                .authenticationSuccessHandler(authenticationSuccessHandler)
                 .and()
                 .logout()
                 .logoutUrl("/logout")
@@ -69,6 +87,47 @@ public class SecurityConfig {
         successHandler.setLogoutSuccessUrl(URI.create(uri));
         return successHandler;
     }
+
+    public static class MyEntryPoint implements ServerAuthenticationEntryPoint {
+
+        final HttpStatus httpStatus;
+        final ServerRequestCache requestCache;
+        public MyEntryPoint(HttpStatus httpStatus, ServerRequestCache webSessionServerRequestCache) {
+            this.httpStatus = httpStatus;
+            this.requestCache = webSessionServerRequestCache;
+        }
+
+        @Override
+        public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
+            return this.requestCache.saveRequest(exchange)
+                    .then(Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(this.httpStatus)));
+        }
+    }
+
+    public static class MyServerRedirectStrategy implements ServerRedirectStrategy {
+        private HttpStatus httpStatus = HttpStatus.FOUND;
+
+        public Mono<Void> sendRedirect(ServerWebExchange exchange, URI location) {
+            Assert.notNull(exchange, "exchange cannot be null");
+            Assert.notNull(location, "location cannot be null");
+            return Mono.fromRunnable(() -> {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(this.httpStatus);
+                response.getHeaders().setLocation(createLocation(exchange, location));
+            });
+        }
+
+        private URI createLocation(ServerWebExchange exchange, URI location) {
+            String url = location.toASCIIString();
+            final String api = "/api";
+            if (url.startsWith(api)) {
+                String substring = url.substring(api.length());
+                return URI.create(substring);
+            }
+            return location;
+        }
+    }
+
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
