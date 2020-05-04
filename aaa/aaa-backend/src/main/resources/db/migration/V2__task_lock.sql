c878cccec792d58d06755e8e3a41d533ee9ab561
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-04 22:06:15 +0300

    savepoint

--- aaa/aaa-backend/src/main/resources/db/migration/V2__task_lock.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS locks;
-
-CREATE TABLE locks.task_lock(
-  name varchar(64) PRIMARY KEY NOT NULL,
-  lock_until timestamp NULL,
-  locked_at timestamp NULL,
-  locked_by varchar(255) NOT NULL
-);
