c0bb4f7ab69dd15616a9472e381c1751ea07b401
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-04 21:50:31 +0300

    cleanup entities

--- aaa/aaa-backend/src/main/java/com/github/nkonev/blog/security/BlogSecurityService.java
+++ aaa/aaa-backend/src/main/java/com/github/nkonev/blog/security/BlogSecurityService.java
@@ -5,12 +5,8 @@ import com.github.nkonev.blog.dto.CommentDTO;
 import com.github.nkonev.blog.dto.LockDTO;
 import com.github.nkonev.blog.dto.PostDTO;
 import com.github.nkonev.blog.dto.UserAccountDetailsDTO;
-import com.github.nkonev.blog.entity.jdbc.Comment;
-import com.github.nkonev.blog.entity.jdbc.Post;
 import com.github.nkonev.blog.entity.jdbc.UserAccount;
 import com.github.nkonev.blog.dto.UserRole;
-import com.github.nkonev.blog.repository.jdbc.CommentRepository;
-import com.github.nkonev.blog.repository.jdbc.PostRepository;
 import com.github.nkonev.blog.repository.jdbc.UserAccountRepository;
 import com.github.nkonev.blog.security.permissions.CommentPermissions;
 import com.github.nkonev.blog.security.permissions.PostPermissions;
@@ -31,98 +27,7 @@ public class BlogSecurityService {
     private RoleHierarchy roleHierarchy;
 
     @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
-    private CommentRepository commentRepository;
-
-    @Autowired
     private UserAccountRepository userAccountRepository;
-
-    public boolean hasPostPermission(PostDTO dto, UserAccountDetailsDTO userAccount, PostPermissions permission) {
-        Assert.notNull(dto, "PostDTO can't be null");
-        return hasPostPermission(dto.getId(), userAccount, permission);
-    }
-
-    private Post getPostOrException(long id) {
-        Post post = postRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Post with id "+id+" not found"));
-        return post;
-    }
-
-    public boolean hasPostPermission(long id, UserAccountDetailsDTO userAccount, PostPermissions permission) {
-        Post post = getPostOrException(id);
-        return hasPostPermission(post, userAccount, permission);
-    }
-
-    public boolean hasPostPermission(UserAccountDetailsDTO userAccount, PostPermissions permission) {
-        return hasPostPermission((Post)null, userAccount, permission);
-    }
-
-
-    public boolean hasPostPermission(Post post, UserAccountDetailsDTO userAccount, PostPermissions permission) {
-        if (userAccount == null) {return false;}
-
-        if (permission == PostPermissions.CREATE) {
-            return true;
-        }
-
-        if (permission == PostPermissions.READ_MY) {
-            return true;
-        }
-
-        if (post == null) {
-            return false;
-        }
-
-        if (roleHierarchy.getReachableGrantedAuthorities(userAccount.getAuthorities()).contains(new SimpleGrantedAuthority(UserRole.ROLE_MODERATOR.name()))){
-            return true;
-        }
-        if (post.getOwnerId().equals(userAccount.getId()) && permission==PostPermissions.EDIT){
-            return true;
-        }
-        return false;
-    }
-
-
-
-
-
-    public boolean hasCommentPermission(CommentDTO dto, UserAccountDetailsDTO userAccount, CommentPermissions permission) {
-        Assert.notNull(dto, "CommentDTO can't be null");
-        return hasCommentPermission(dto.getId(), userAccount, permission);
-    }
-
-    public boolean hasCommentPermission(long id, UserAccountDetailsDTO userAccount, CommentPermissions permission) {
-        Comment comment = commentRepository.findById(id).orElseThrow(()->new IllegalArgumentException("Comment with id "+id+" not found"));
-        return hasCommentPermission(comment, userAccount, permission);
-    }
-
-    public boolean hasCommentPermission(UserAccountDetailsDTO userAccount, CommentPermissions permission) {
-        return hasCommentPermission((Comment)null, userAccount, permission);
-    }
-
-    public boolean hasCommentPermission(Comment comment, UserAccountDetailsDTO userAccount, CommentPermissions permission) {
-        if (userAccount == null) {
-            return false;
-        }
-
-        if (permission == CommentPermissions.CREATE) {
-            return true;
-        }
-
-        if (comment == null) {
-            return false;
-        }
-
-        if (roleHierarchy.getReachableGrantedAuthorities(userAccount.getAuthorities()).contains(new SimpleGrantedAuthority(UserRole.ROLE_MODERATOR.name()))){
-            return true;
-        }
-        if (Long.valueOf(comment.getOwnerId()).equals(userAccount.getId())){
-            return true;
-        }
-
-        return false;
-    }
 
     public boolean hasSessionManagementPermission(UserAccountDetailsDTO userAccount) {
         if (userAccount==null){
--- aaa/aaa-backend/src/main/java/com/github/nkonev/blog/services/UserDeleteService.java
+++ aaa/aaa-backend/src/main/java/com/github/nkonev/blog/services/UserDeleteService.java
@@ -2,8 +2,6 @@ package com.github.nkonev.blog.services;
 
 import com.github.nkonev.blog.Constants;
 import com.github.nkonev.blog.entity.jdbc.UserAccount;
-import com.github.nkonev.blog.repository.jdbc.CommentRepository;
-import com.github.nkonev.blog.repository.jdbc.PostRepository;
 import com.github.nkonev.blog.repository.jdbc.UserAccountRepository;
 import com.github.nkonev.blog.security.BlogUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +13,13 @@ public class UserDeleteService {
     private BlogUserDetailsService blogUserDetailsService;
 
     @Autowired
-    private CommentRepository commentRepository;
-    @Autowired
     private UserAccountRepository userAccountRepository;
-
-    @Autowired
-    private PostRepository postRepository;
 
     public long deleteUser(long userId) {
         blogUserDetailsService.killSessions(userId);
         UserAccount deleted = userAccountRepository.findByUsername(Constants.DELETED).orElseThrow();
-        postRepository.moveToAnotherUser(userId, deleted.getId());
-        commentRepository.moveToAnotherUser(userId, deleted.getId());
+//        postRepository.moveToAnotherUser(userId, deleted.getId());
+//        commentRepository.moveToAnotherUser(userId, deleted.getId());
         userAccountRepository.deleteById(userId);
 
         return userAccountRepository.count();
