ec3a30190f5bde29082a959efba7fe89f3bb4d3b
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-12 04:36:40 +0300

    reconfigure sleuth for gw

--- gateway/src/main/java/name/nkonev/gateway/GatewayApplication.java
+++ gateway/src/main/java/name/nkonev/gateway/GatewayApplication.java
@@ -2,12 +2,10 @@ package name.nkonev.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.instrument.messaging.TraceMessagingAutoConfiguration;
-import org.springframework.cloud.sleuth.instrument.messaging.TraceSpringIntegrationAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.tools.agent.ReactorDebugAgent;
 
-@SpringBootApplication(exclude = {TraceMessagingAutoConfiguration.class, TraceSpringIntegrationAutoConfiguration.class})
+@SpringBootApplication
 @RestController
 public class GatewayApplication {
 
