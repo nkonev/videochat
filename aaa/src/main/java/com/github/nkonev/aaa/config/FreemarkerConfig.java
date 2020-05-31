7eab5218cb6281476a8367a7aa4b23f66a28244c
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-31 05:48:01 +0300

    add test login page to aaa

---
+++ aaa/src/main/java/com/github/nkonev/aaa/config/FreemarkerConfig.java
@@ -0,0 +1,23 @@
+package com.github.nkonev.aaa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
+/**
+ * https://vorba.ch/2018/spring-boot-freemarker-security-jsp-taglib.html
+ */
+@Configuration
+public class FreemarkerConfig {
+
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @PostConstruct
+    public void postConstruct() {
+        freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(Arrays.asList("/META-INF/security.tld", "/META-INF/spring-form.tld"));
+    }
+}
---
+++ aaa/src/main/java/com/github/nkonev/aaa/controllers/FreemarkerController.java
@@ -0,0 +1,24 @@
+package com.github.nkonev.aaa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FreemarkerController {
+
+    @GetMapping({"/index.html", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping({"/oauth2.html"})
+    public String oauth2() {
+        return "oauth2";
+    }
+
+    @GetMapping({"/login.html"})
+    public String login() {
+        return "login";
+    }
+
+}
