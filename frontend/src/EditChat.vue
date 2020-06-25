b769ab8b323cd76c05c78fc9c24d71b8a3cdf52a
Nikita Konev <nikit.cpp@yandex.ru>
2020-06-25 04:00:26 +0300

    non-persistent

--- frontend/src/EditChat.vue
+++ frontend/src/EditChat.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row justify="center">
-        <v-dialog v-model="show" persistent max-width="290">
+        <v-dialog v-model="show" max-width="290">
             <v-card>
                 <v-card-title class="headline">Create chat</v-card-title>
                 <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
