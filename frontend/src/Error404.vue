028cb2a80f9bca4e55fe2b071ba63a22415614fc
Nikita Konev <nikit.cpp@yandex.ru>
2020-07-07 21:24:21 +0300

    fix 404

--- frontend/src/Error404.vue
+++ frontend/src/Error404.vue
@@ -1,5 +1,11 @@
-<script>
-    export default {
-        template: `<div>404 Page not found ;(</div>`
-    };
-</script>
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12">
+                <v-row justify="center" align="center">
+                    Page not found
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
