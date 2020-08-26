bb25fd9341bbdcd1cd47640aac2a0a2f675c2c40
Nikita Konev <nikit.cpp@yandex.ru>
2020-08-27 01:42:12 +0300

    remove still unused file_metadata

--- storage/db/file.go
+++ storage/db/file.go
@@ -1,10 +1,7 @@
 package db
 
 import (
-	"errors"
-	"github.com/google/uuid"
 	. "nkonev.name/storage/logger"
-	"time"
 )
 
 // Go enum
@@ -14,31 +11,7 @@ const (
 	AVATAR_200x200 AvatarType = "AVATAR_200x200"
 )
 
-// db model
-type FileMetadata struct {
-	Id                 uuid.UUID
-	FileName           string
-	LastUpdateDateTime time.Time
-}
-
-func (tx *Tx) CreateFileMetadata(u *FileMetadata, userId int64) (uuid.UUID, error) {
-	// Validate the input.
-	if u == nil {
-		return uuid.New(), errors.New("file_metadata required")
-	} else if u.FileName == "" {
-		return uuid.New(), errors.New("file name required")
-	}
-
-	res := tx.QueryRow(`INSERT INTO file_metadata(file_name, owner_id) VALUES ($1, $2) RETURNING id`, u.FileName, userId)
-	var id uuid.UUID
-	if err := res.Scan(&id); err != nil {
-		Logger.Errorf("Error during creating file metadata %v", err)
-		return uuid.New(), err
-	}
-	return id, nil
-}
-
-func (tx *Tx) CreateAvatarMetadata(userId int64, avatarType AvatarType, filename string) (error) {
+func (tx *Tx) CreateAvatarMetadata(userId int64, avatarType AvatarType, filename string) error {
 	_, err := tx.Exec(`INSERT INTO avatar_metadata(avatar_type, owner_id, file_name) VALUES ($1, $2, $3) ON CONFLICT(owner_id, avatar_type) DO NOTHING`, avatarType, userId, filename)
 	if err != nil {
 		Logger.Errorf("Error during creating avatar metadata %v", err)
--- storage/db/migrations/000020_create_storage_table.up.sql
+++ storage/db/migrations/000020_create_storage_table.up.sql
@@ -2,13 +2,6 @@ CREATE TYPE avatar_type AS ENUM (
     'AVATAR_200x200'
 );
 
-CREATE TABLE file_metadata(
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    file_name VARCHAR(1024) NOT NULL,
-    owner_id BIGINT NOT NULL,
-    create_date_time TIMESTAMP NOT NULL DEFAULT utc_now()
-);
-
 CREATE TABLE avatar_metadata(
     owner_id BIGINT NOT NULL,
     avatar_type avatar_type NOT NULL,
