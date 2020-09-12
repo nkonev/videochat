8760927c63e279d4ec75b6a567565c986dc583ae
Nikita Konev <nikit.cpp@yandex.ru>
2020-09-13 01:10:00 +0300

    rename FreemarkerController -> DebugController

--- aaa/src/main/java/com/github/nkonev/aaa/controllers/DebugController.java
+++ aaa/src/main/java/com/github/nkonev/aaa/controllers/DebugController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FreemarkerController {
+public class DebugController {
 
     @GetMapping({"/index.html", "/"})
     public String index() {
