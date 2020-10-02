6d92b902942632614023722aa93f6888297b03db
Nikita Konev <nikit.cpp@yandex.ru>
2020-10-03 01:40:48 +0300

    infinity loop caused 130 exit code

--- chat/listener/redis_listener_config.go
+++ chat/listener/redis_listener_config.go
@@ -221,74 +221,76 @@ func RedisAaaConnection(lc fx.Lifecycle) (*redis.Pool, error) {
 func ListenPubSubChannels(
 	pool *redis.Pool,
 	onMessage AaaUserProfileUpdateListener,
-	lc fx.Lifecycle) error {
+	lc fx.Lifecycle) {
 
-	lifecycleHookAppended := false
+	go func() {
+		lifecycleHookAppended := false
 
-	var psc redis.PubSubConn
-	for {
-		conn := pool.Get()
-		defer conn.Close()
+		var psc redis.PubSubConn
+		for {
+			conn := pool.Get()
+			defer conn.Close()
 
-		Logger.Infof("Starting redis aaa subscription")
-		var channels []string = []string{"user.profile.update"}
+			Logger.Infof("Starting redis aaa subscription")
+			var channels []string = []string{"user.profile.update"}
 
-		psc = redis.PubSubConn{Conn: conn}
+			psc = redis.PubSubConn{Conn: conn}
 
-		if err := psc.Subscribe(redis.Args{}.AddFlat(channels)...); err != nil {
-			Logger.Errorf("Error on aaa subscription %v", err)
-			sleepBetweenRetry()
-			continue
-		}
+			if err := psc.Subscribe(redis.Args{}.AddFlat(channels)...); err != nil {
+				Logger.Errorf("Error on aaa subscription %v", err)
+				sleepBetweenRetry()
+				continue
+			}
 
-		if !lifecycleHookAppended {
-			// Signal the receiving goroutine to exit by unsubscribing from all channels.
-			lc.Append(fx.Hook{
-				OnStop: func(ctx context.Context) error {
-					Logger.Infof("Removing redis aaa subscription")
-					return psc.Unsubscribe()
-				},
-			})
-			lifecycleHookAppended = true
-		}
+			if !lifecycleHookAppended {
+				// Signal the receiving goroutine to exit by unsubscribing from all channels.
+				lc.Append(fx.Hook{
+					OnStop: func(ctx context.Context) error {
+						Logger.Infof("Removing redis aaa subscription")
+						return psc.Unsubscribe()
+					},
+				})
+				lifecycleHookAppended = true
+			}
 
-		done := make(chan error, 1)
+			done := make(chan error, 1)
 
-		// Start a goroutine to receive notifications from the server.
-		go func() {
-			for {
-				switch n := psc.Receive().(type) {
-				case error:
-					done <- n
-					return
-				case redis.Message:
-					if err := onMessage(n.Channel, n.Data); err != nil {
-						done <- err
+			// Start a goroutine to receive notifications from the server.
+			go func() {
+				for {
+					switch n := psc.Receive().(type) {
+					case error:
+						done <- n
 						return
-					}
-				case redis.Subscription:
-					switch n.Count {
-					case len(channels):
-						// Notify application when all channels are subscribed.
-						Logger.Infof("app subscribed to the all channels")
-					case 0:
-						// Return from the goroutine when all channels are unsubscribed.
-						done <- nil
-						return
+					case redis.Message:
+						if err := onMessage(n.Channel, n.Data); err != nil {
+							done <- err
+							return
+						}
+					case redis.Subscription:
+						switch n.Count {
+						case len(channels):
+							// Notify application when all channels are subscribed.
+							Logger.Infof("app subscribed to the all channels")
+						case 0:
+							// Return from the goroutine when all channels are unsubscribed.
+							done <- nil
+							return
+						}
 					}
 				}
-			}
-		}()
+			}()
 
-		// Wait for goroutine to complete.
-		err := <-done
-		Logger.Errorf("Error on redis aaa subscription %v", err)
+			// Wait for goroutine to complete.
+			err := <-done
+			Logger.Errorf("Error on redis aaa subscription %v", err)
 
-		err = psc.Unsubscribe()
-		Logger.Infof("Unsubscribing, error=%v", err)
+			err = psc.Unsubscribe()
+			Logger.Infof("Unsubscribing, error=%v", err)
 
-		sleepBetweenRetry()
-	}
+			sleepBetweenRetry()
+		}
+	}()
 }
 
 func sleepBetweenRetry() {
