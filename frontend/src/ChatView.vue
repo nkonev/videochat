63553f9a01960dded3fe7e5970b99f51997fe70a
Nikita Konev <nikit.cpp@yandex.ru>
2021-01-23 05:12:51 +0300

    use official wide pane splitter

--- frontend/src/ChatView.vue
+++ frontend/src/ChatView.vue
@@ -55,7 +55,7 @@
     import { Splitpanes, Pane } from 'splitpanes'
     import {getCorrectUserAvatar} from "./utils";
     import MessageItem from "./MessageItem";
-    import 'splitpanes/dist/splitpanes.css';
+    // import 'splitpanes/dist/splitpanes.css';
 
     export default {
         mixins: [infinityListMixin()],
@@ -296,4 +296,21 @@
 
 </style>
 
+<style lang="stylus">
+.splitpanes {background-color: #f8f8f8;}
 
+.splitpanes__splitter {background-color: #ccc;position: relative;}
+.splitpanes__splitter:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    transition: opacity 0.4s;
+    background-color: rgba(255, 0, 0, 0.3);
+    opacity: 0;
+    z-index: 1;
+}
+.splitpanes__splitter:hover:before {opacity: 1;}
+.splitpanes--vertical > .splitpanes__splitter:before {left: -30px;right: -30px;height: 100%;}
+.splitpanes--horizontal > .splitpanes__splitter:before {top: -30px;bottom: -30px;width: 100%;}
+</style>
