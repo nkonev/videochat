4c80e1b474e707a9e19e4603e397fa59860d2b4a
Nikita Konev <nikit.cpp@yandex.ru>
2021-02-27 21:37:57 +0300

    another attempt to set vp9

--- frontend/src/ChatVideo.vue
+++ frontend/src/ChatVideo.vue
@@ -56,6 +56,7 @@
         methods: {
             joinSession(configObj) {
                 const config = {
+                    codec: 'vp9',
                     iceServers: configObj.ICEServers.map((iceServConf)=>{
                         const result = {
                             urls: iceServConf.URLs
@@ -207,6 +208,7 @@
                 return LocalStream.getUserMedia({
                   resolution: "vga",
                   audio: true,
+                  codec: 'vp9',
                 }).then((media) => {
                   this.localMedia = media
                   this.$refs.localVideoComponent.setSource(media);
@@ -219,6 +221,7 @@
             getAndPublishScreen() {
                 return LocalStream.getDisplayMedia({
                   audio: true,
+                  codec: 'vp9',
                 }).then((media) => {
                     this.localMedia = media;
                     this.localMedia.unmute("audio");
