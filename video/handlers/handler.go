898be60206f0e3da0a331760b207a1c736ee2644
Nikita Konev <nikit.cpp@yandex.ru>
2021-03-05 01:36:50 +0300

    initial kick

--- video/handlers/handler.go
+++ video/handlers/handler.go
@@ -5,9 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
+	log "github.com/pion/ion-log"
 	"github.com/pion/ion-sfu/cmd/signal/json-rpc/server"
 	"github.com/pion/ion-sfu/pkg/sfu"
-	log "github.com/pion/ion-log"
+	"github.com/pion/webrtc/v3"
 	"github.com/sourcegraph/jsonrpc2"
 	websocketjsonrpc2 "github.com/sourcegraph/jsonrpc2/websocket"
 	"io/fs"
@@ -16,18 +17,19 @@ import (
 	"nkonev.name/video/config"
 	"strings"
 	"sync"
+	"time"
 )
 
 //go:embed static
 var embeddedFiles embed.FS
 
 type Handler struct {
-	client *http.Client
-	upgrader *websocket.Upgrader
-	sessionUserPeer *sync.Map
-	sfu *sfu.SFU
-	conf *config.ExtendedConfig
-	httpFs *http.FileSystem
+	client    *http.Client
+	upgrader  *websocket.Upgrader
+	userPeers *sync.Map // it contains all peers of a particular user, across all the sessions
+	sfu       *sfu.SFU
+	conf      *config.ExtendedConfig
+	httpFs    *http.FileSystem
 }
 
 func NewHandler(
@@ -42,16 +44,28 @@ func NewHandler(
 	}
 	staticDir := http.FS(fsys)
 
-	return Handler{
-		client:          client,
-		upgrader:        upgrader,
-		sessionUserPeer: &sync.Map{},
-		sfu:             sfu,
-		conf:            conf,
-		httpFs: 		&staticDir,
+	handler := Handler{
+		client:    client,
+		upgrader:  upgrader,
+		userPeers: &sync.Map{},
+		sfu:       sfu,
+		conf:      conf,
+		httpFs:    &staticDir,
 	}
+	// TODO to lifecycle methods
+	go func() {
+		for _, session := range sfu.GetSessions() {
+			for _, peer := range session.Peers() {
+				if handler.peerIsClosed(peer) {
+					handler.removePeer(peer)
+				}
+			}
+		}
+		oneSecond, _ := time.ParseDuration("1s")
+		time.Sleep(oneSecond)
+	}()
+	return handler
 }
-
 
 func (h *Handler) SfuHandler(w http.ResponseWriter, r *http.Request) {
 	userId := r.Header.Get("X-Auth-UserId")
@@ -68,9 +82,8 @@ func (h *Handler) SfuHandler(w http.ResponseWriter, r *http.Request) {
 	defer c.Close()
 
 	p := server.NewJSONSignal(sfu.NewPeer(h.sfu))
-	addPeerToMap(h.sessionUserPeer, chatId, userId, p)
+	h.addPeerToMap(userId, p.Peer)
 	defer p.Close()
-	defer removePeerFromMap(h.sessionUserPeer, chatId, userId)
 
 	jc := jsonrpc2.NewConn(r.Context(), websocketjsonrpc2.NewObjectStream(c), p)
 	<-jc.DisconnectNotify()
@@ -85,7 +98,7 @@ func (h *Handler) Users(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	chatInterface, ok := h.sessionUserPeer.Load(chatId)
+	chatInterface, ok := h.userPeers.Load(chatId)
 	response := UsersResponse{}
 	if ok {
 		chat := chatInterface.(*sync.Map)
@@ -113,10 +126,13 @@ func (h *Handler) NotifyChatParticipants(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	var usersCount int64 = 0
-	chatInterface, ok := h.sessionUserPeer.Load(chatId)
-	if ok {
-		chat := chatInterface.(*sync.Map)
-		usersCount = countMapLen(chat)
+	session, _ := h.sfu.GetSession(fmt.Sprintf("chat%v", chatId))
+	if session != nil {
+		for _, peer:= range session.Peers() {
+			if h.peerIsAlive(peer) {
+				usersCount++
+			}
+		}
 	}
 
 	url0 := h.conf.ChatConfig.ChatUrlConfig.Base
@@ -142,6 +158,20 @@ func (h *Handler) NotifyChatParticipants(w http.ResponseWriter, r *http.Request)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	}
+}
+
+func (h *Handler) peerIsAlive(peer *sfu.Peer) bool {
+	if peer == nil {
+		return false
+	}
+	return peer.Publisher().SignalingState() != webrtc.SignalingStateClosed
+}
+
+func (h *Handler) peerIsClosed(peer *sfu.Peer) bool {
+	if peer == nil {
+		return false
+	}
+	return peer.Publisher().SignalingState() == webrtc.SignalingStateClosed
 }
 
 // GET `/api/video/config`
@@ -192,29 +222,73 @@ func (h *Handler) checkAccess(client *http.Client, userIdString string, chatIdSt
 	}
 }
 
-func addPeerToMap(sessionUserPeer *sync.Map, chatId string, userId string, peer *server.JSONSignal) {
-	userPeerInterface, _ := sessionUserPeer.LoadOrStore(chatId, &sync.Map{})
-	userPeer := userPeerInterface.(*sync.Map)
-	log.Infof("Storing peer for userId=%v to chatId=%v", userId, chatId)
-	userPeer.Store(userId, peer)
+// GET `/internal/kick`
+func (h *Handler) Kick(w http.ResponseWriter, r *http.Request) {
+	chatId := r.URL.Query().Get("chatId")
+	userToKickId := r.URL.Query().Get("userId")
+	h.kick(chatId, userToKickId)
 }
 
-func removePeerFromMap(sessionUserPeer *sync.Map, chatId string, userId string) {
-	userPeerInterface, ok := sessionUserPeer.Load(chatId)
-	if !ok {
-		log.Errorf("Cannot remove chatId=%v from sessionUserPeer", chatId)
+type UserPeers struct {
+	sync.RWMutex
+	Peers []*sfu.Peer
+}
+
+func (h *Handler) kick(chatId, userId string) {
+	// for peer := session.peers
+	session, _ := h.sfu.GetSession(fmt.Sprintf("chat%v", chatId)) // ChatVideo.vue
+	if session == nil {
 		return
 	}
-	userPeer := userPeerInterface.(*sync.Map)
-	log.Infof("Removing peer for userId=%v from chatId=%v", userId, chatId)
-	userPeer.Delete(userId)
-
-	userPeerLength := countMapLen(userPeer)
-
-	if userPeerLength == 0 {
-		log.Infof("For chatId=%v there is no peers, removing user %v", chatId, userId)
-		sessionUserPeer.Delete(chatId)
+	for _, peerF := range session.Peers() {
+		// if getUserId(peer) == userId
+		if h.hasPeer(userId, peerF) {
+			// session.disconnect(peer)
+			session.RemovePeer(peerF.ID())
+		}
 	}
+}
+
+func (h *Handler) addPeerToMap(userId string, peer *sfu.Peer) {
+	userPeerInterface, _ := h.userPeers.LoadOrStore(userId, &UserPeers{})
+	userPeer := userPeerInterface.(*UserPeers)
+	log.Infof("Storing peer for userId=%v", userId)
+	userPeer.Lock()
+	defer userPeer.Unlock()
+	userPeer.Peers = append(userPeer.Peers, peer)
+}
+
+func (h *Handler) hasPeer(userId string, peer *sfu.Peer) bool {
+	if peer == nil {
+		return false
+	}
+	if load, ok := h.userPeers.Load(userId); ok {
+		userPeer := load.(*UserPeers)
+		userPeer.RLock()
+		defer userPeer.RUnlock()
+		for _, enumerablePeer := range userPeer.Peers {
+			if enumerablePeer.ID() == peer.ID() {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Handler) removePeer(peer *sfu.Peer) {
+	if peer == nil {
+		return
+	}
+	h.userPeers.Range(func(_, enumerableUserI interface{}) bool {
+		enumerableUser := enumerableUserI.(*UserPeers)
+		enumerableUser.Lock()
+		defer enumerableUser.Unlock()
+		for _, userPeer := range enumerableUser.Peers {
+			userPeer
+		}
+		return true
+	})
+
 }
 
 func countMapLen(m *sync.Map) int64 {
