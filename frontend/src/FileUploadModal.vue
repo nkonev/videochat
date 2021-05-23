9640209f57234b39a34664f6c7e5e8b42959eaa8
Nikita Konev <nikit.cpp@yandex.ru>
2021-05-23 18:53:02 +0300

    upload progress

--- frontend/src/FileUploadModal.vue
+++ frontend/src/FileUploadModal.vue
@@ -4,10 +4,31 @@
             <v-card>
                 <v-card-title>Upload files</v-card-title>
 
-                <v-card-text>Choose files</v-card-text>
+                <v-container>
+                    <v-file-input
+                        :disabled="uploading"
+                        counter
+                        multiple
+                        show-size
+                        small-chips
+                        truncate-length="15"
+                    ></v-file-input>
+
+                    <v-progress-linear
+                        class="mt-2"
+                        v-if="uploading"
+                        v-model="totalProgress"
+                        color="success"
+                        buffer-value="0"
+                        value="20"
+                        stream
+                    >
+                    </v-progress-linear>
+                </v-container>
 
                 <v-card-actions class="pa-4">
-                    <v-btn class="mr-4" @click="hideModal()">Close</v-btn>
+                    <v-btn color="primary" :loading="uploading" @click="upload()" :disabled="uploading">Upload</v-btn>
+                    <v-btn class="mr-4" @click="hideModal()" :disabled="uploading">Close</v-btn>
                     <v-spacer/>
                 </v-card-actions>
             </v-card>
@@ -21,17 +42,28 @@ import bus, {OPEN_FILE_UPLOAD_MODAL, CLOSE_FILE_UPLOAD_MODAL} from "./bus";
 export default {
     data () {
         return {
+            uploading: false,
             show: false,
         }
     },
     methods: {
         showModal(chatId) {
-            console.info("ChatId", chatId);
             this.$data.show = true;
         },
         hideModal() {
             this.$data.show = false;
         },
+        upload() {
+            this.uploading = true;
+            setTimeout(()=>{
+                this.uploading = false;
+            }, 5000);
+        }
+    },
+    computed: {
+        totalProgress() {
+            return 25;
+        }
     },
     created() {
         bus.$on(OPEN_FILE_UPLOAD_MODAL, this.showModal);
