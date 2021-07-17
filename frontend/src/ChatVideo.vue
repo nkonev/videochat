bbeae7f13534479747ff794cf51bcb37002b576b
Nikita Konev <nikit.cpp@yandex.ru>
2021-07-17 18:40:40 +0300

    unpublish localMediaStream on session leaving

--- frontend/src/ChatVideo.vue
+++ frontend/src/ChatVideo.vue
@@ -173,6 +173,7 @@
                 if (this.localMediaStream) {
                     this.localMediaStream.getTracks().forEach(t => t.stop());
                 }
+                this.localMediaStream.unpublish();
                 if (this.clientLocal) {
                     this.clientLocal.close();
                 }
