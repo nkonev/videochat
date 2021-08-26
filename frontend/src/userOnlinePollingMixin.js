076c42773eb3f57301affa1cee7f4b1f93e42eb8
Nikita Konev <3160384+nkonev@users.noreply.github.com>
2021-08-27 00:01:32 +0300

    fix behaviour when in startPolling(participantsProvider, handler) got empty participants array

--- frontend/src/userOnlinePollingMixin.js
+++ frontend/src/userOnlinePollingMixin.js
@@ -9,9 +9,16 @@ export default () => {
         methods: {
             startPolling(participantsProvider, handler) {
                 intervalId = setInterval(()=>{
+                    const participants = participantsProvider();
+                    if (!participants || participants.length == 0) {
+                        console.debug("Participants are empty or participantsProvider returned equal null, invoking handler with empty array");
+                        handler([]);
+                        return;
+                    }
+                    console.debug("Participants are non-empty, invoking axios");
                     axios.get(`/api/chat/online`, {
                         params: {
-                            participantIds: participantsProvider().reduce((f, s) => `${f},${s}`)
+                            participantIds: participants.reduce((f, s) => `${f},${s}`)
                             // participantIds: [1,2,3].reduce((f, s) => `${f},${s}`)
                         }
                     }).then(value => {
