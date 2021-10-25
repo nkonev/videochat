ad14f6db461b554888856b348e04b39e72a8969a
Nikita Konev <nikit.cpp@yandex.ru>
2021-10-25 23:44:33 +0300

    make ValidationError record

--- aaa/src/main/java/com/github/nkonev/aaa/dto/ValidationError.java
+++ aaa/src/main/java/com/github/nkonev/aaa/dto/ValidationError.java
@@ -3,40 +3,8 @@ package com.github.nkonev.aaa.dto;
 /**
  * Slice of FieldError
  */
-public class ValidationError {
-    private String field;
-    private Object rejectedValue;
-    private String message;
-
-    public ValidationError() { }
-
-    public ValidationError(String field, Object rejectedValue, String message) {
-        this.field = field;
-        this.rejectedValue = rejectedValue;
-        this.message = message;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
-
-    public Object getRejectedValue() {
-        return rejectedValue;
-    }
-
-    public void setRejectedValue(Object rejectedValue) {
-        this.rejectedValue = rejectedValue;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-}
+public record ValidationError (
+    String field,
+    Object rejectedValue,
+    String message
+) { }
