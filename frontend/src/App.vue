a4737f69ae856b2123781afc8ebfef142e43def7
Nikita Konev <nikit.cpp@yandex.ru>
2021-10-29 04:07:36 +0300

    fix icon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -72,7 +72,7 @@
                 </v-list-item>
 
                 <v-list-item @click="openVideoSettings()" v-if="shouldDisplayVideoSettings()">
-                    <v-list-item-icon><v-icon>mdi-account</v-icon></v-list-item-icon>
+                    <v-list-item-icon><v-icon>mdi-cog</v-icon></v-list-item-icon>
                     <v-list-item-content><v-list-item-title>{{ $vuetify.lang.t('$vuetify.video_settings') }}</v-list-item-title></v-list-item-content>
                 </v-list-item>
 
