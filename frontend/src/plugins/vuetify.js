554a6fc6a9abbce143f001e9d3357cb01855c76f
Nikita Konev <nikit.cpp@yandex.ru>
2021-10-30 02:15:36 +0300

    change locale folder

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -2,8 +2,8 @@ import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loade
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework' // https://vuetifyjs.com/en/features/sass-variables/#compilation-time
 
-import en from './localeEn';
-import ru from './localeRu';
+import en from './locale/en';
+import ru from './locale/ru';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
