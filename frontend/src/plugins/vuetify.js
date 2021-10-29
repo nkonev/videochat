3c02c65821710dbf0496acd6e0c3107144276937
Nikita Konev <nikit.cpp@yandex.ru>
2021-10-30 02:19:32 +0300

    reorder in vuetify.js

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,13 +4,14 @@ import Vuetify from 'vuetify/lib/framework' // https://vuetifyjs.com/en/features
 
 import en from './locale/en';
 import ru from './locale/ru';
+import { getStoredLanguage } from "@/utils";
 
+// https://www.npmjs.com/package/@fortawesome/vue-fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook'
 import { faVk } from '@fortawesome/free-brands-svg-icons/faVk'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons/faGoogle'
-import {getStoredLanguage} from "@/utils";
 
 library.add(faFacebook, faVk, faGoogle);
 Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
