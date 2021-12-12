5956341d95cc30e13610b0bd2e43dff095fbcf4a
Nikita Konev <nikit.cpp@yandex.ru>
2021-12-12 15:07:51 +0300

    migrate user avatars to new location

--- aaa/src/main/resources/db/migration/V8__update_avatar.sql
+++ aaa/src/main/resources/db/migration/V8__update_avatar.sql
@@ -1,2 +1,2 @@
 UPDATE users SET avatar = replace(avatar, '/storage/public/avatar', '/storage/public/user/avatar');
-UPDATE users SET avatar_big = replace(avatar_big, 'old_text', 'new_text');
+UPDATE users SET avatar_big = replace(avatar_big, '/storage/public/avatar', '/storage/public/user/avatar');
