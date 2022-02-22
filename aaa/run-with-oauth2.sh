b5a639c07d0778850f653d0dc8b0f9479e68b13b
Nikita Konev <nikit.cpp@yandex.ru>
2022-02-22 05:16:50 +0300

    get rid of logback-test.xml

--- aaa/run-with-oauth2.sh
+++ aaa/run-with-oauth2.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 # You need to start EmulatorServersController firstly
+# export JAVA_HOME=/usr/lib/jvm/bellsoft-java17.x86_64
+# ./mvnw clean package -DskipTests
 exec $JAVA_HOME/bin/java -jar target/aaa-0.0.0-exec.jar --spring.config.location=file:src/main/resources/config/application.yml,file:src/test/resources/config/oauth2-basic.yml
