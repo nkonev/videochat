5cfdbbb5f71d91de0c20e90abe5cb7727ec8bd1e
Nikita Konev <nikit.cpp@yandex.ru>
2022-06-18 04:45:32 +0400

    Fix incorrect using of Map which lead to closing all on closing one screen share although video isn't closed

--- frontend/src/ChatVideoUserComponentHolder.js
+++ frontend/src/ChatVideoUserComponentHolder.js
@@ -2,15 +2,16 @@ export class ChatVideoUserComponentHolder {
     #userVideoComponents = new Map();
 
     addComponentForUser(userIdentity, component) {
-        let existingList = this.#userVideoComponents[userIdentity];
+        let existingList = this.#userVideoComponents.get(userIdentity);
         if (!existingList) {
-            existingList = this.#userVideoComponents[userIdentity] = [];
+            this.#userVideoComponents.set(userIdentity, []);
+            existingList = this.#userVideoComponents.get(userIdentity);
         }
         existingList.push(component);
     }
 
     removeComponentForUser(userIdentity, component) {
-        let existingList = this.#userVideoComponents[userIdentity];
+        let existingList = this.#userVideoComponents.get(userIdentity);
         if (existingList) {
             for(let i = 0; i < existingList.length; i++){
                 if (existingList[i].getId() == component.getId()) {
@@ -19,7 +20,7 @@ export class ChatVideoUserComponentHolder {
             }
         }
         if (existingList.length == 0) {
-            this.#userVideoComponents.delete(userIdentity)
+            this.#userVideoComponents.delete(userIdentity);
         }
     }
 
@@ -28,25 +29,12 @@ export class ChatVideoUserComponentHolder {
     }
 
     getByUser(userIdentity) {
-        let existingList = this.#userVideoComponents[userIdentity];
+        let existingList = this.#userVideoComponents.get(userIdentity);
         if (!existingList) {
-            existingList = this.#userVideoComponents[userIdentity] = [];
+            this.#userVideoComponents.set(userIdentity, []);
+            existingList = this.#userVideoComponents.get(userIdentity);
         }
         return existingList;
     }
 
-    /**
-     * Very heavy complexity
-     * @param callback
-     */
-    invokeOnAllComponents(callback) {
-        for (const userId in this.#userVideoComponents) {
-            const existingList = this.#userVideoComponents[userId];
-            if (existingList) {
-                for(const component of existingList){
-                    callback(component);
-                }
-            }
-        }
-    }
 }
