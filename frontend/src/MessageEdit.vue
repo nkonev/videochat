55cbeaf00a6796febeaac046488343ac431c4667
Nikita Konev <nikit.cpp@yandex.ru>
2022-08-07 07:15:43 +0400

    Restore and change delta 90px to 56px in MessageEdit.vue

--- frontend/src/MessageEdit.vue
+++ frontend/src/MessageEdit.vue
@@ -220,7 +220,7 @@
         computed: {
             ...mapGetters({currentUser: GET_USER}),
             messageEditHeight() {
-                return this.fullHeight ? 'calc(100vh - 56px)' : '100%'
+                return this.fullHeight ? 'calc(100vh - 56px - 56px)' : '100%'
             },
         },
         mounted() {
