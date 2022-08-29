bcfb1602d54b47ea2b7e6ac65313c3f685051c09
Nikita Konev <nikit.cpp@yandex.ru>
2022-08-29 12:29:23 +0400

    restore canBroadcast in MessageEdit.vue

--- frontend/src/MessageEdit.vue
+++ frontend/src/MessageEdit.vue
@@ -67,7 +67,7 @@
                                 </v-badge>
                             </template>
                             <v-btn icon tile width="48px" class="mr-2" @click="resetInput()" :title="$vuetify.lang.t('$vuetify.message_edit_clear')"><v-icon>mdi-delete</v-icon></v-btn>
-                            <v-switch dense hide-details class="ma-0 mr-4" v-model="sendBroadcast"
+                            <v-switch v-if="canBroadcast" dense hide-details class="ma-0 mr-4" v-model="sendBroadcast"
                                 :label="$vuetify.lang.t('$vuetify.message_broadcast')"
                             ></v-switch>
                             <v-btn color="primary" @click="sendMessageToChat" tile class="mr-0" :title="$vuetify.lang.t('$vuetify.message_edit_send')"><v-icon color="white">mdi-send</v-icon></v-btn>
