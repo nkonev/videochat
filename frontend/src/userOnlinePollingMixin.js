4644cc37eb113563f53fc877f93de68fcd390bab
Nikita Konev <nikit.cpp@yandex.ru>
2022-09-10 06:32:10 +0400

    Faster getting user online statuses

--- frontend/src/userOnlinePollingMixin.js
+++ frontend/src/userOnlinePollingMixin.js
@@ -7,23 +7,27 @@ export default () => {
 
     return  {
         methods: {
-            startPolling(participantsProvider, handler) {
-                intervalId = setInterval(()=>{
-                    const participants = participantsProvider();
-                    if (!participants || participants.length == 0) {
-                        console.debug("Participants are empty or participantsProvider returned equal null, invoking handler with empty array");
-                        handler([]);
-                        return;
+            invokeApiUserOnline(participantsProvider, handler) {
+                const participants = participantsProvider();
+                if (!participants || participants.length == 0) {
+                    console.debug("Participants are empty or participantsProvider returned equal null, invoking handler with empty array");
+                    handler([]);
+                    return;
+                }
+                console.debug("Participants are non-empty, invoking axios");
+                axios.get(`/api/user/online`, {
+                    params: {
+                        userId: participants.reduce((f, s) => `${f},${s}`)
+                        // participantIds: [1,2,3].reduce((f, s) => `${f},${s}`)
                     }
-                    console.debug("Participants are non-empty, invoking axios");
-                    axios.get(`/api/user/online`, {
-                        params: {
-                            userId: participants.reduce((f, s) => `${f},${s}`)
-                            // participantIds: [1,2,3].reduce((f, s) => `${f},${s}`)
-                        }
-                    }).then(value => {
-                        handler(value.data)
-                    })
+                }).then(value => {
+                    handler(value.data)
+                })
+            },
+            startPolling(participantsProvider, handler) {
+                this.invokeApiUserOnline(participantsProvider, handler);
+                intervalId = setInterval(()=>{
+                    this.invokeApiUserOnline(participantsProvider, handler);
                 }, pollingInterval);
             },
             stopPolling() {
