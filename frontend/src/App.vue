697cb0c40494b7b330c11e3f9b3c83af45657a15
Nikita Konev <nikit.cpp@yandex.ru>
2022-11-23 14:04:21 +0400

    Added switches

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -144,24 +144,156 @@
 
                 <v-card>
                     <v-list class="pb-0">
-                        <v-list-item>
+                        <v-list-item link>
                             <v-list-item-icon class="mr-4"><v-icon>mdi-message</v-icon></v-list-item-icon>
-                            <v-list-item-content>
+                            <v-list-item-content class="py-0">
                                 <v-list-item-title>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum i[r]ure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</v-list-item-title>
                                 <v-list-item-subtitle>Mention</v-list-item-subtitle>
                             </v-list-item-content>
                         </v-list-item>
 
-                        <v-list-item>
+                        <v-list-item link>
                             <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
-                            <v-list-item-content>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
+                                <v-list-item-subtitle>Missed call</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item link>
+                            <v-list-item-icon class="mr-4"><v-icon>mdi-phone</v-icon></v-list-item-icon>
+                            <v-list-item-content class="py-0">
                                 <v-list-item-title>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</v-list-item-title>
                                 <v-list-item-subtitle>Missed call</v-list-item-subtitle>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list>
-
-                    <v-card-actions class="pt-0">
+                    <v-divider/>
+                    <v-card-actions>
+                        <v-switch
+                            label="Mentions"
+                            dense
+                            hide-details
+                            class="ma-0 ml-2 mr-4"
+                        ></v-switch>
+                        <v-switch
+                            label="Missed calls"
+                            dense
+                            hide-details
+                            class="ma-0 ml-2 mr-4"
+                        ></v-switch>
                         <v-spacer></v-spacer>
 
                         <v-btn
