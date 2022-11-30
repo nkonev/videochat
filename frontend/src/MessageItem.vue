ad8ca4fce705187e958197705338e67bbb33fe22
Nikita Konev <nikit.cpp@yandex.ru>
2022-12-01 02:26:31 +0400

    Fix hash link in MessageItem on mobile

--- frontend/src/MessageItem.vue
+++ frontend/src/MessageItem.vue
@@ -12,7 +12,7 @@
                 <v-icon class="mx-1 ml-2" v-if="item.fileItemUuid" @click="onFilesClicked(item.fileItemUuid)" small>mdi-file-download</v-icon>
                 <v-icon class="mx-1" v-if="item.canEdit" color="error" @click="deleteMessage(item)" dark small>mdi-delete</v-icon>
                 <v-icon class="mx-1" v-if="item.canEdit" color="primary" @click="editMessage(item)" dark small>mdi-lead-pencil</v-icon>
-                <a class="mx-1" :href="require('./routes').chat + '/' + chatId + require('./routes').messageIdHashPrefix + item.id">#</a>
+                <a class="mx-1 hash" :href="require('./routes').chat + '/' + chatId + require('./routes').messageIdHashPrefix + item.id">#</a>
             </v-container>
             <div @click="onMessageClick(item)" @mousemove="onMessageMouseMove(item)" class="pa-0 ma-0 mt-1 message-item-wrapper" :class="{ my: my, highlight: highlight }" >
                 <v-container v-html="item.text" class="ma-0 pre-formatted message-item-text"></v-container>
@@ -140,6 +140,11 @@
   }
   .my {
       background $messageSelectedBackground
+  }
+
+  .hash {
+      align-items: center;
+      display: inline-flex;
   }
 
   .highlight {
