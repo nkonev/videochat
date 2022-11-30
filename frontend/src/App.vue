b742bc92276e58cb405de4094bd97e9c54979185
Nikita Konev <nikit.cpp@yandex.ru>
2022-12-01 01:22:17 +0400

    Better handle focus on mobile

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -117,7 +117,7 @@
                 <v-icon>mdi-magnify</v-icon>
             </v-btn>
             <v-card light v-if="isShowSearch && !showSearchButton || !isMobile()" :width="isMobile() ? '100%' : ''">
-                <v-text-field prepend-icon="mdi-magnify" hide-details single-line v-model="searchString" :label="searchName" clearable clear-icon="mdi-close-circle" @keyup.esc="resetInput"></v-text-field>
+                <v-text-field :autofocus="isMobile()" prepend-icon="mdi-magnify" hide-details single-line v-model="searchString" :label="searchName" clearable clear-icon="mdi-close-circle" @keyup.esc="resetInput" @blur="showSearchButton=true"></v-text-field>
             </v-card>
 
             <template v-if="showSearchButton || !isMobile()">
