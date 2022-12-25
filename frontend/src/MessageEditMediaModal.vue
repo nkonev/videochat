f5b5d7b64970a332ed90cbc4c84629fe0cf0a672
Nikita Konev <nikit.cpp@yandex.ru>
2022-12-25 04:56:01 +0400

    Use hover

--- frontend/src/MessageEditMediaModal.vue
+++ frontend/src/MessageEditMediaModal.vue
@@ -11,23 +11,31 @@
                             :key="card.title"
                             :cols="card.flex"
                         >
-                            <v-card>
-                                <v-img
-                                    :src="card.src"
-                                    class="white--text align-end"
-                                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                                    height="200px"
-                                >
-                                    <v-card-title v-text="card.title"></v-card-title>
-                                </v-img>
+                            <v-hover>
+                                <template v-slot:default="{ hover }">
+                                    <v-card>
+                                        <v-img
+                                            :src="card.src"
+                                            class="white--text align-end"
+                                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                                            height="200px"
+                                        >
+                                            <v-card-title v-text="card.title"></v-card-title>
+                                        </v-img>
 
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn @click="accept()">
-                                        {{ $vuetify.lang.t('$vuetify.choose') }}
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
+                                        <v-fade-transition>
+                                            <v-overlay
+                                                v-if="hover"
+                                                absolute
+                                                @click="accept()"
+                                                style="cursor: pointer"
+                                            >
+                                            </v-overlay>
+                                        </v-fade-transition>
+
+                                    </v-card>
+                                </template>
+                            </v-hover>
                         </v-col>
                     </v-row>
                 </v-card-text>
