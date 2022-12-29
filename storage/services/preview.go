af9eb0ca0135a16cb0295550f7a5f6d11e0d93cb
Nikita Konev <nikit.cpp@yandex.ru>
2022-12-29 23:55:09 +0400

    Image preview

--- storage/services/preview.go
+++ storage/services/preview.go
@@ -3,7 +3,10 @@ package services
 import (
 	"bytes"
 	"context"
+	"github.com/disintegration/imaging"
 	"github.com/minio/minio-go/v7"
+	"image"
+	"image/jpeg"
 	"net/url"
 	"nkonev.name/storage/dto"
 	. "nkonev.name/storage/logger"
@@ -31,7 +34,34 @@ func (s PreviewService) HandleMinioEvent(data *dto.MinioEvent) {
 	normalizedKey := utils.StripBucketName(data.Key, s.minioConfig.Files)
 	if strings.HasPrefix(data.EventName, utils.ObjectCreated) {
 		if utils.IsImage(normalizedKey) {
-			// TODO image preview
+			object, err := s.minio.GetObject(ctx, s.minioConfig.Files, normalizedKey, minio.GetObjectOptions{})
+			if err != nil {
+				Logger.Errorf("Error during getting image file %v for %v", err, normalizedKey)
+				return
+			}
+			srcImage, _, err := image.Decode(object)
+			if err != nil {
+				Logger.Errorf("Error during decoding image: %v", err)
+				return
+			}
+
+			dstImage := imaging.Resize(srcImage, 400, 300, imaging.Lanczos)
+			byteBuffer := new(bytes.Buffer)
+			err = jpeg.Encode(byteBuffer, dstImage, nil)
+			if err != nil {
+				Logger.Errorf("Error during encoding image: %v", err)
+				return
+			}
+
+			newKey := utils.FilesIdToFilesPreviewId(normalizedKey, s.minioConfig)
+			newKey = utils.SetImagePreviewExtension(newKey)
+
+			var objectSize int64 = int64(byteBuffer.Len())
+			_, err = s.minio.PutObject(ctx, s.minioConfig.FilesPreview, newKey, byteBuffer, objectSize, minio.PutObjectOptions{ContentType: "image/jpg"})
+			if err != nil {
+				Logger.Errorf("Error during storing thumbnail %v for %v", err, normalizedKey)
+				return
+			}
 		} else if utils.IsVideo(normalizedKey) {
 			d, _ := time.ParseDuration("10m")
 			presignedUrl, err := s.minio.PresignedGetObject(ctx, s.minioConfig.Files, normalizedKey, d, url.Values{})
