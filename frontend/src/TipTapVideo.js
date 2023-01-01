f80d827f0a447ead0677ee08a1c236e3038a32b8
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-01 17:29:03 +0400

    Make video inline

--- frontend/src/TipTapVideo.js
+++ frontend/src/TipTapVideo.js
@@ -3,10 +3,11 @@ import { Node, mergeAttributes } from '@tiptap/core';
 // https://www.codemzy.com/blog/tiptap-video-embed-extension
 const Video = Node.create({
     name: 'video', // unique name for the Node
-    group: 'block', // belongs to the 'block' group of extensions
+    group: 'inline',
     selectable: true, // so we can select the video
     draggable: true, // so we can drag the video
     atom: true, // is a single unit
+    inline: true,
 
     parseHTML() {
         return [
