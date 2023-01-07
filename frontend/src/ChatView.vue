415ba1fb41a341ee363094d6f6c2be62454ee9d2
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-07 17:45:24 +0400

    Less aggressive downloading when click scroll down

--- frontend/src/ChatView.vue
+++ frontend/src/ChatView.vue
@@ -478,7 +478,7 @@
             },
             onClickScrollDown() {
                 // condition is a dummy heuristic (because right now doe to outdated vue-infinite-loading we cannot scroll down several times. nevertheless I think it's a pretty good heuristic so I think it worth to remain it here after updating to vue 3 and another modern infinity scroller)
-                if (this.items.length <= pageSize && !this.getRouteHash()) {
+                if (this.items.length <= pageSize * 2 && !this.getRouteHash()) {
                     this.scrollDown();
                 } else {
                     this.resetVariables();
