52d81a5bf60fc341944a3072412a4669cc6ac39c
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-19 04:04:00 +0400

    Also watch for videochat_name

--- frontend/src/ChatView.vue
+++ frontend/src/ChatView.vue
@@ -806,7 +806,7 @@
                 // reacts on user manually typing hash - in this case we may trigger reload if we don't have the necessary message
                 handler: function(newRoute, oldRoute) {
                     console.debug("Watched on newRoute", newRoute, " oldRoute", oldRoute);
-                    if (newRoute.name === chat_name) {
+                    if (newRoute.name === chat_name || newRoute.name === videochat_name) {
                         this.setHashVariables();
                         if (this.hasInitialHash) {
                             if (findIndexNonStrictly(this.items, {id: this.highlightMessageId}) === -1) {
