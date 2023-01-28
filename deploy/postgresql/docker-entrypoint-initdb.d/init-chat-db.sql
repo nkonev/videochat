b0e3cf808ff349e081c52d981d401f1541a6e0a5
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-28 04:34:14 +0400

    Upd init chat db

--- deploy/postgresql/docker-entrypoint-initdb.d/init-chat-db.sql
+++ deploy/postgresql/docker-entrypoint-initdb.d/init-chat-db.sql
@@ -1,12 +1,12 @@
 -- ALTER SYSTEM SET max_connections = 400;
 -- Uncomment if you need to view the full postgres logs (SQL statements, ...) via `docker logs -f postgresql-test`
 ALTER SYSTEM SET log_statement = 'all';
-ALTER SYSTEM SET synchronous_commit = 'off'; -- https://postgrespro.ru/docs/postgrespro/9.5/runtime-config-wal.html#GUC-SYNCHRONOUS-COMMIT
+-- ALTER SYSTEM SET synchronous_commit = 'off'; -- https://postgrespro.ru/docs/postgrespro/9.5/runtime-config-wal.html#GUC-SYNCHRONOUS-COMMIT
 -- ALTER SYSTEM SET shared_buffers='512MB';
-ALTER SYSTEM SET fsync=FALSE;
-ALTER SYSTEM SET full_page_writes=FALSE;
-ALTER SYSTEM SET commit_delay=100000;
-ALTER SYSTEM SET commit_siblings=10;
+-- ALTER SYSTEM SET fsync=FALSE;
+-- ALTER SYSTEM SET full_page_writes=FALSE;
+-- ALTER SYSTEM SET commit_delay=100000;
+-- ALTER SYSTEM SET commit_siblings=10;
 -- ALTER SYSTEM SET work_mem='50MB';
 ALTER SYSTEM SET log_line_prefix = '%a %u@%d ';
 
