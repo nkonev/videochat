6caab1348a7b8f98404ed3485e97946b35d5ef37
Nikita Konev <nikit.cpp@yandex.ru>
2023-05-06 18:35:52 +0400

    BlogPost layout

--- frontend/src/BlogPost.vue
+++ frontend/src/BlogPost.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
         <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h1>
-        <div class="message-item-text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+        <div class="pr-1 mr-1 pl-1 mt-0 message-item-root" >
+            <div class="message-item-with-buttons-wrapper">
+                <v-list-item class="grow">
+                    <v-list-item-avatar color="grey darken-3">
+                        <v-img
+                            class="elevation-6"
+                            alt=""
+                            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                        ></v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                        <v-list-item-title>Nikita Konev</v-list-item-title><v-list-item-title>at 2022-12-30</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+
+                <div class="pa-0 ma-0 mt-1 message-item-wrapper my">
+                    <v-container v-html="text" class="message-item-text ml-0"></v-container>
+                </div>
+            </div>
         </div>
-        <v-divider/>
+
         <v-list>
             <!-- TODO MessageList.vue -->
             <template v-for="(item, index) in items">
@@ -17,7 +32,7 @@
                     :key="item.id"
                     :item="item"
                     :chatId="chatId"
-                    :my="true"
+                    :my="item.my"
                 ></MessageItem>
             </template>
         </v-list>
@@ -31,6 +46,8 @@
         data() {
             return {
                 chatId: 34,
+                text: "<p>Lorem upsum</p><p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>" +
+                      "<p>Lorem upsum</p><p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
                 items: [
                     {
                         "id": 2822,
@@ -49,7 +66,8 @@
                         "canDelete": true,
                         "fileItemUuid": "17b13878-2a0d-4860-a6d6-2ad8545901b0",
                         "embedMessage": null,
-                        "pinned": false
+                        "pinned": false,
+                        "my": false,
                     },
                     {
                         "id": 2823,
@@ -68,7 +86,8 @@
                         "canDelete": true,
                         "fileItemUuid": "17b13878-2a0d-4860-a6d6-2ad8545901b0",
                         "embedMessage": null,
-                        "pinned": false
+                        "pinned": false,
+                        "my": false,
                     },
                     {
                         "id": 2824,
@@ -87,7 +106,8 @@
                         "canDelete": true,
                         "fileItemUuid": "17b13878-2a0d-4860-a6d6-2ad8545901b0",
                         "embedMessage": null,
-                        "pinned": false
+                        "pinned": false,
+                        "my": true,
                     },
                     {
                         "id": 2825,
@@ -106,7 +126,8 @@
                         "canDelete": true,
                         "fileItemUuid": "17b13878-2a0d-4860-a6d6-2ad8545901b0",
                         "embedMessage": null,
-                        "pinned": false
+                        "pinned": false,
+                        "my": true,
                     },
                 ]
             }
