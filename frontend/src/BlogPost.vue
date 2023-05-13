cb9b5b278f555f26f7acf799057322f6651c5319
Nikita Konev <nikit.cpp@yandex.ru>
2023-05-13 07:26:21 +0400

    Add shadow

--- frontend/src/BlogPost.vue
+++ frontend/src/BlogPost.vue
@@ -162,5 +162,10 @@
         background white
         border-color $borderColor
         border-style: dashed;
+        border-width 1px
+        box-shadow:
+            0 0 0 1px rgba(0, 0, 0, 0.05),
+            0px 10px 20px rgba(0, 0, 0, 0.1)
+
     }
 </style>
