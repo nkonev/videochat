403eb5be0fe35c6d6a34a73708ea7fb74313d232
Nikita Konev <nikit.cpp@yandex.ru>
2023-05-14 21:20:33 +0400

    Improve layout of main blog screen

--- frontend/src/BlogApp.vue
+++ frontend/src/BlogApp.vue
@@ -7,8 +7,12 @@
             </v-breadcrumbs>
 
             <v-spacer></v-spacer>
-            <v-card v-if="isShowSearch" light :width="isMobile() ? '100%' : ''">
-                <v-text-field prepend-icon="mdi-magnify" hide-details single-line v-model="searchString" :label="searchName" clearable clear-icon="mdi-close-circle" @keyup.esc="resetInput"></v-text-field>
+
+            <v-btn v-if="isShowSearch && showSearchButton && isMobile()" icon :title="searchName" @click="onOpenSearch()">
+                <v-icon>{{ hasSearchString ? 'mdi-magnify-close' : 'mdi-magnify'}}</v-icon>
+            </v-btn>
+            <v-card v-if="isShowSearch && !showSearchButton || !isMobile()" light :width="isMobile() ? '100%' : ''">
+                <v-text-field prepend-icon="mdi-magnify" hide-details single-line v-model="searchString" :label="searchName" clearable clear-icon="mdi-close-circle" @keyup.esc="resetInput" @blur="showSearchButton=true"></v-text-field>
             </v-card>
         </v-app-bar>
 
@@ -31,15 +35,18 @@ import {GET_SEARCH_NAME, GET_SEARCH_STRING, GET_SHOW_SEARCH, SET_SEARCH_STRING} 
 import {mapGetters} from 'vuex'
 import bus, {SEARCH_STRING_CHANGED} from "@/blogBus";
 import {blog, blog_post_name} from "@/blogRoutes";
+import {hasLength} from "@/utils";
 
 let unsubscribe;
 
 export default {
     data: () => ({
+        showSearchButton: true,
     }),
     methods: {
         resetInput() {
             this.searchString = null;
+            this.showSearchButton = true;
         },
         getBreadcrumbs() {
             const ret = [
@@ -66,6 +73,9 @@ export default {
             }
             return ret
         },
+        onOpenSearch() {
+            this.showSearchButton = false;
+        },
     },
     computed: {
         searchString: {
@@ -81,11 +91,17 @@ export default {
             searchName: GET_SEARCH_NAME,
             isShowSearch: GET_SHOW_SEARCH,
         }),
+        hasSearchString() {
+            return hasLength(this.searchString)
+        }
     },
     created() {
         unsubscribe = this.$store.subscribe((mutation, state) => {
             if (mutation.type == SET_SEARCH_STRING) {
                 bus.$emit(SEARCH_STRING_CHANGED, mutation.payload);
+                if (!hasLength(mutation.payload)) {
+                    this.showSearchButton = true;
+                }
             }
         });
     },
