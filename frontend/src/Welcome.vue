f33f9f16f18a8d4ed6e6f5e2b4ad33669508b050
Nikita Konev <nikit.cpp@yandex.ru>
2023-07-17 02:53:23 +0400

    Full fix Welcome (behaviour was introduced in 029237e9)

--- frontend/src/Welcome.vue
+++ frontend/src/Welcome.vue
@@ -1,26 +1,28 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-card-title class="d-flex justify-space-around">{{$vuetify.lang.t('$vuetify.welcome_participant', currentUser.login)}}</v-card-title>
-            <v-card-actions class="d-flex justify-space-around flex-wrap flex-row">
-                <v-btn color="primary" @click="createChat()" text>
-                    <v-icon>mdi-plus</v-icon>
-                    {{ $vuetify.lang.t('$vuetify.new_chat') }}
-                </v-btn>
-                <v-btn @click="findUser()" text>
-                    <v-icon>mdi-magnify</v-icon>
-                    {{ $vuetify.lang.t('$vuetify.find_user') }}
-                </v-btn>
-                <v-btn @click="availableChats()" text>
-                    <v-icon>mdi-forum</v-icon>
-                    {{ $vuetify.lang.t('$vuetify.public_chats') }}
-                </v-btn>
-                <v-btn @click="goBlog()" text>
-                    <v-icon>mdi-postage-stamp</v-icon>
-                    {{ $vuetify.lang.t('$vuetify.blogs') }}
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+    <v-container fill-height fluid :style="heightWithoutAppBar">
+        <v-row align="center" justify="center">
+            <v-card>
+                <v-card-title class="d-flex justify-space-around">{{$vuetify.lang.t('$vuetify.welcome_participant', currentUser.login)}}</v-card-title>
+                <v-card-actions class="d-flex justify-space-around flex-wrap flex-row">
+                    <v-btn color="primary" @click="createChat()" text class="my-2">
+                        <v-icon>mdi-plus</v-icon>
+                        {{ $vuetify.lang.t('$vuetify.new_chat') }}
+                    </v-btn>
+                    <v-btn @click="findUser()" text class="my-2">
+                        <v-icon>mdi-magnify</v-icon>
+                        {{ $vuetify.lang.t('$vuetify.find_user') }}
+                    </v-btn>
+                    <v-btn @click="availableChats()" text class="my-2">
+                        <v-icon>mdi-forum</v-icon>
+                        {{ $vuetify.lang.t('$vuetify.public_chats') }}
+                    </v-btn>
+                    <v-btn @click="goBlog()" text class="my-2">
+                        <v-icon>mdi-postage-stamp</v-icon>
+                        {{ $vuetify.lang.t('$vuetify.blogs') }}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-row>
     </v-container>
 </template>
 
@@ -28,10 +30,14 @@
 import {GET_USER, SET_SEARCH_STRING} from "@/store";
     import {mapGetters} from "vuex";
     import bus, {OPEN_CHAT_EDIT, OPEN_FIND_USER} from "@/bus";
-import {publicallyAvailableForSearchChatsQuery} from "@/utils";
-import {blog} from "@/routes";
+    import {publicallyAvailableForSearchChatsQuery} from "@/utils";
+    import {blog} from "@/routes";
+    import heightMixin from "@/heightMixin";
 
     export default {
+        mixins: [
+            heightMixin()
+        ],
         computed: {
             ...mapGetters({currentUser: GET_USER}),
         },
