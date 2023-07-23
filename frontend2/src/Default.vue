e20c6667e8a86213518555b69332d1e4420d54ff
Nikita Konev <nikit.cpp@yandex.ru>
2023-07-23 06:01:54 +0400

    Initial implementation with Intersection Observer API

--- frontend2/src/Default.vue
+++ frontend2/src/Default.vue
@@ -2,6 +2,7 @@
 
     <v-container style="height: calc(100vh - 64px); background: darkgrey">
         <div class="my-scroller">
+          <div class="first-element" style="min-height: 1px; background: #9cffa1"></div>
           <div v-for="chat in chats" :key="chats.id" class="card mb-3">
             <div class="row g-0">
               <div class="col">
@@ -14,8 +15,8 @@
               </div>
             </div>
           </div>
+          <div class="last-element" style="min-height: 1px; background: #c62828"></div>
 
-          <InfiniteLoading @infinite="load" top></InfiniteLoading>
         </div>
 
     </v-container>
@@ -23,8 +24,13 @@
 </template>
 
 <script>
-    import InfiniteLoading from "@/lib/infinite-scrolling/components/InfiniteLoading.vue";
     // import "v3-infinite-loading/lib/style.css"; //required if you're not going to override default slots
+
+
+    const css_str= (el) => {
+      return el.tagName.toLowerCase() + (el.id ? '#' + el.id : "") + '.' + (Array.from(el.classList)).join('.')
+    };
+
 
     const PAGE_SIZE = 5;
 
@@ -58,13 +64,47 @@
           })
         }
       },
-      components: {
-        InfiniteLoading
-      },
       mounted() {
         // for(let i=0; i<1000; ++i) {
         //   this.chats.push({id: 1, avatar: "", name: "adsa" + i});
         // }
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+        let options = {
+          root: document.querySelector(".my-scroller"),
+          rootMargin: "0px",
+          threshold: 1.0,
+        };
+        let callback = (entries, observer) => {
+          entries.forEach((entry) => {
+            // Each entry describes an intersection change for one observed
+            // target element:
+            //   entry.boundingClientRect
+            //   entry.intersectionRatio
+            //   entry.intersectionRect
+            //   entry.isIntersecting
+            //   entry.rootBounds
+            //   entry.target
+            //   entry.time
+            console.log("Entry: intersecting=", entry.isIntersecting, css_str(entry.target));
+          });
+        };
+        let observer = new IntersectionObserver(callback, options);
+
+        let target1 = document.querySelector(".first-element");
+        observer.observe(target1);
+
+        let target2 = document.querySelector(".last-element");
+        observer.observe(target2);
+
+        this.load({
+          complete() {
+            console.log("Complete");
+          },
+          loaded() {
+            console.log("Loaded");
+          }
+        });
         console.log("this.chats", this.chats)
       }
     }
