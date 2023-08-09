894c0d6f511f45c39cfbcc2578fa658f0ac1cb64
Nikita Konev <nikit.cpp@yandex.ru>
2023-08-09 12:13:46 +0400

    Change behaviour of debounce

--- frontend2/src/mixins/infiniteScrollMixin.js
+++ frontend2/src/mixins/infiniteScrollMixin.js
@@ -166,7 +166,7 @@ export default (name) => {
           }
         };
 
-        const observerCallback = debounce(observerCallback0, 700, {leading:false, trailing:true}); // 200 is for eliminating double loads in Firefox
+        const observerCallback = debounce(observerCallback0, 700, {leading:true, trailing:true}); // 200 is for eliminating double loads in Firefox
 
         this.observer = new IntersectionObserver(observerCallback, options);
         this.observer.observe(document.querySelector(this.scrollerSelector() + " " + this.bottomElementSelector()));
