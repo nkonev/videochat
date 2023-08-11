573091befce5a3f17be5b5c491fb12d6c2a061da
Nikita Konev <nikit.cpp@yandex.ru>
2023-08-12 03:31:58 +0400

    Fix norma behaviour of saving scroll

--- frontend2/src/MessageList.vue
+++ frontend2/src/MessageList.vue
@@ -296,6 +296,7 @@
             this.saveLastVisibleElement(oldVal);
 
             if (hasLength(newVal)) {
+              this.hasLoadedHash = hasLength(this.loadedMessageId); // reinit boolean flag
               await this.reloadItems();
             }
           },
