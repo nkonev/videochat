c483040c70d5527cfb7a56d9d094ca7e4c948b30
Nikita Konev <nikit.cpp@yandex.ru>
2023-08-16 05:13:56 +0400

    Improve look of LoginModal according https://vuetifyjs.com/en/components/buttons/#dialog-action

--- frontend2/src/LoginModal.vue
+++ frontend2/src/LoginModal.vue
@@ -5,8 +5,7 @@
     -->
     <v-row justify="center">
         <v-dialog persistent v-model="show" max-width="440">
-            <v-card>
-                <v-card-title :class="isMobile() ? 'pa-4 pb-2 headline' : 'headline'">{{ $vuetify.locale.t('$vuetify.login_title') }}</v-card-title>
+            <v-card :title="$vuetify.locale.t('$vuetify.login_title')">
 
                 <v-card-text :class="isMobile() ? 'pa-4 pt-0' : 'pt-0'">
                     <v-form
