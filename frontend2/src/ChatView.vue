nkonev/videochat

commit aca740bcdec4679d4afa1cd6ac7646477ffd1d5e

New mobile-frieldly splitpanes
Better, fix hazf-active horizontal

MessageList width 100%

Move it to scss file

Load the dynamically

Move it to create()

Better

frontend2/src/ChatView.vue
@@ -18,7 +18,8 @@
 
 <script>
 import { Splitpanes, Pane } from 'splitpanes'
-import 'splitpanes/dist/splitpanes.css'
+// import 'splitpanes/dist/splitpanes.css'
+//import '@/splitpanes-mobile.scss'
 import ChatList from "@/ChatList.vue";
 import MessageList from "@/MessageList.vue";
 import MessageEdit from "@/MessageEdit.vue";
@@ -29,6 +30,9 @@ import axios from "axios";
 import {hasLength, offerToJoinToPublicChatStatus} from "@/utils";
 import bus, {PROFILE_SET, VIDEO_CALL_USER_COUNT_CHANGED} from "@/bus/bus";
 import {chat_list_name} from "@/router/routes";
+
+const webSplitpanesCss = () => import('splitpanes/dist/splitpanes.css');
+const mobileSplitpanesCss = () => import("@/splitpanes-mobile.scss");
 
 const chatDtoFactory = () => {
   return {
@@ -134,8 +138,14 @@ export default {
       }
     },
   },
+  created() {
+    if (this.isMobile()) {
+      mobileSplitpanesCss()
+    } else {
+      webSplitpanesCss()
+    }
+  },
   async mounted() {
-
     this.chatStore.title = `Chat #${this.chatId}`;
     this.chatStore.chatUsersCount = 0;
     this.chatStore.isShowSearch = true;

frontend2/src/MessageList.vue
@@ -369,6 +369,7 @@
 <style lang="stylus">
     .my-messages-scroller {
       height 100%
+      width: 100%
       overflow-y scroll !important
       display flex
       flex-direction column-reverse

frontend2/src/splitpanes-mobile.scss
@@ -1,0 +1,36 @@
+.splitpanes{
+  display:flex;
+  width:100%;
+  height:100%
+}
+.splitpanes--vertical{flex-direction:row}
+.splitpanes--horizontal{flex-direction:column}
+.splitpanes--dragging *{user-select:none}
+.splitpanes__pane{width:100%;height:100%;overflow:hidden}
+
+.splitpanes {
+  background-color: #f2f2f2;
+
+  &__pane {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  &__splitter {background-color: #ccc;position: relative;}
+
+  &__splitter:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    transition: 0.4s;
+  }
+  &--vertical > &__splitter:before {left: -30px;right: -30px; z-index: 10;}
+  &--horizontal > &__splitter:before {top: -30px;bottom: -30px; z-index: 10;}
+  &--vertical > &__splitter {cursor: col-resize;}
+  &--horizontal > &__splitter {cursor: row-resize;}
+  &__splitter:hover:before {background-color: rgba(255, 0, 0, 0.3);}
+}

commit af0bd81deec721dfc78250365a53ae738b1521c0

Fix restoring scroll (either hash or saved scroll)
from bug, introduced in aca740bc

frontend2/src/ChatView.vue
@@ -29,9 +29,6 @@ import {hasLength, offerToJoinToPublicChatStatus} from "@/utils";
 import bus, {PROFILE_SET, VIDEO_CALL_USER_COUNT_CHANGED} from "@/bus/bus";
 import {chat_list_name} from "@/router/routes";
 
-const webSplitpanesCss = () => import('splitpanes/dist/splitpanes.css');
-const mobileSplitpanesCss = () => import("@/splitpanes-mobile.scss");
-
 const chatDtoFactory = () => {
   return {
     participantIds:[],
@@ -137,11 +134,7 @@ export default {
     },
   },
   created() {
-    if (this.isMobile()) {
-      mobileSplitpanesCss()
-    } else {
-      webSplitpanesCss()
-    }
+
   },
   async mounted() {
     this.chatStore.title = `Chat #${this.chatId}`;

frontend2/src/main.js
@@ -23,6 +23,22 @@ import FontAwesomeIcon from "@/plugins/faIcons";
 axios.defaults.xsrfCookieName = "VIDEOCHAT_XSRF_TOKEN";
 axios.defaults.xsrfHeaderName = "X-XSRF-TOKEN";
 
+const webSplitpanesCss = () => import('splitpanes/dist/splitpanes.css');
+const mobileSplitpanesCss = () => import("@/splitpanes-mobile.scss");
+
+// it's placed here, before app creation
+// otherwise, if we put it into ChatView.created()
+// it is going to break MessageList.scrollTo()
+// to check -
+// 1. scroll to certain message
+// 2. reload the page
+// 3. top message should be the same after page reloading
+if (isMobileBrowser()) {
+  mobileSplitpanesCss()
+} else {
+  webSplitpanesCss()
+}
+
 const chatStore = useChatStore();
 
 export function registerPlugins (app) {