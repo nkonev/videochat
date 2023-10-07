06e95d37871581a47a2987a62d0341cfe8dc0655
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-07 20:02:41 +0400

    make run with oauth2 the same as development md

--- aaa/run-with-oauth2.sh
+++ aaa/run-with-oauth2.sh
@@ -2,4 +2,4 @@
 # You need to start EmulatorServersController firstly
 # export JAVA_HOME=/usr/lib/jvm/bellsoft-java17.x86_64
 ./mvnw clean package -DskipTests
-exec $JAVA_HOME/bin/java -jar target/aaa-0.0.0-exec.jar --spring.config.location=file:src/main/resources/config/application.yml,file:src/test/resources/config/oauth2-basic.yml,file:src/test/resources/config/user-test-controller.yml,file:src/test/resources/config/demo-migration.yml
+exec $JAVA_HOME/bin/java -jar target/aaa-0.0.0-exec.jar --spring.config.location=file:src/main/resources/config/application.yml,file:src/test/resources/config/oauth2-basic.yml,file:src/test/resources/config/oauth2-keycloak.yml,file:src/test/resources/config/user-test-controller.yml,file:src/test/resources/config/demo-migration.yml,file:src/test/resources/config/log-email.yml
