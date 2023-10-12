db04fb99ca8b4dcf97b4572a94c822ec95c1d821
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-13 01:46:46 +0400

    Initial BlogPost

--- frontend2/src/BlogPost.vue
+++ frontend2/src/BlogPost.vue
@@ -1,3 +1,163 @@
 <template>
-    This is blog post
+  <div>
+    <h1 v-html="blogDto.title"></h1>
+
+    <div class="pr-1 mr-1 pl-1 mt-0 message-item-root" >
+      <div class="message-item-with-buttons-wrapper">
+        <v-list-item class="grow" v-if="blogDto?.owner">
+          <template v-slot:prepend>
+            <v-avatar :image="blogDto.owner.avatar" @click.prevent="onParticipantClick(blogDto.owner)">
+            </v-avatar>
+          </template>
+
+          <template v-slot:default>
+            <div class="ma-0 pa-0 d-flex top-panel">
+              <div class="author-and-date">
+                <v-list-item-title><a @click.prevent="onParticipantClick(blogDto.owner)" :href="getProfileLink(blogDto.owner)">{{blogDto.owner.login}}</a></v-list-item-title>
+                <v-list-item-subtitle>{{getDate(blogDto.createDateTime)}}</v-list-item-subtitle>
+              </div>
+              <div class="ma-0 pa-0 go-to-chat">
+                <v-btn icon :href="getChatLink()" @click="toChat()" :title="$vuetify.locale.t('$vuetify.go_to_chat')"><v-icon dark>mdi-forum</v-icon></v-btn>
+              </div>
+            </div>
+          </template>
+        </v-list-item>
+
+
+        <div class="pa-0 ma-0 mt-1 message-item-wrapper post-content">
+          <v-container v-html="blogDto.text" class="message-item-text ml-0"></v-container>
+        </div>
+      </div>
+    </div>
+
+    <template v-if="blogDto.messageId">
+      <v-list id="comment-list">
+          <MessageItem v-for="(item, index) in items"
+            :key="item.id"
+            :item="item"
+            :chatId="item.chatId"
+            :isInBlog="true"
+          ></MessageItem>
+      </v-list>
+
+    </template>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+import MessageItem from "@/MessageItem";
+import {getHumanReadableDate, hasLength, replaceOrAppend, setTitle} from "@/utils";
+import {chat, messageIdHashPrefix, profile, profile_name} from "@/router/routes";
+
+const pageSize = 40;
+
+const blogDtoFactory = () => {
+  return {
+    chatId: 0
+  }
+}
+
+export default {
+  data() {
+    return {
+      blogDto: blogDtoFactory(),
+      startingFromItemId: null,
+      page: 0,
+      items: [ ]
+    }
+  },
+  methods: {
+    onParticipantClick(user) {
+      const routeDto = { name: profile_name, params: { id: user.id }};
+      this.$router.push(routeDto);
+    },
+    getProfileLink(user) {
+      let url = profile + "/" + user.id;
+      return url;
+    },
+    getChatLink() {
+      return chat + '/' + this.blogDto.chatId + messageIdHashPrefix + this.blogDto.messageId;
+    },
+    toChat() {
+      window.location.href = this.getChatLink();
+    },
+    getBlog(id) {
+      return axios.get('/api/blog/'+id).then(({data}) => {
+        this.blogDto = data;
+        this.startingFromItemId = data.messageId;
+        setTitle(this.blogDto.title);
+      });
+    },
+    getDate(date) {
+      if (hasLength(date)) {
+        return getHumanReadableDate(date)
+      } else {
+        return null
+      }
+    },
+    getComments(blogId) { // TODO replace with infinity loader
+      if (this.items.length) {
+        this.startingFromItemId = Math.max(...this.items.map(it => it.id));
+        console.log("this.startingFromItemId set to", this.startingFromItemId);
+      }
+
+      axios.get(`/api/blog/${blogId}/comment`, {
+        params: {
+          startingFromItemId: this.startingFromItemId,
+          page: this.page,
+          size: pageSize,
+        },
+      }).then(({ data }) => {
+        const list = data;
+        if (list.length) {
+          this.page += 1;
+          replaceOrAppend(this.items, list);
+        }
+      });
+    },
+  },
+  components: {
+    MessageItem,
+  },
+  mounted() {
+    this.getBlog(this.$route.params.id).then(()=>{
+      this.getComments(this.$route.params.id);
+    });
+  },
+  beforeUnmount() {
+    this.blogDto = blogDtoFactory();
+    this.startingFromItemId = null;
+    this.page = 0;
+    this.items = [];
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "common.styl"
+@import "messageWrapper.styl"
+
+.top-panel {
+  width 100%
+}
+
+.go-to-chat {
+  align-self center
+}
+
+.post-content {
+  background white
+  border-color $borderColor
+  border-style: dashed;
+  border-width 1px
+  box-shadow:
+    0 0 0 1px rgba(0, 0, 0, 0.05),
+    0px 10px 20px rgba(0, 0, 0, 0.1)
+}
+
+.author-and-date {
+  flex: 0 1 auto;
+}
+
+</style>
--- frontend2/src/router/blogRouter.js
+++ frontend2/src/router/blogRouter.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 import {blog, blog_name, blog_post, blog_post_name} from "@/router/blogRoutes";
+import {profile, profile_name} from "@/router/routes";
 
 const routes = [
     {
@@ -11,6 +12,11 @@ const routes = [
         name: blog_post_name,
         path: blog_post + `/:id`,
         component: () => import('@/BlogPost.vue'),
+    },
+    {
+        name: profile_name,
+        path: profile + `/:id`,
+        component: () => import('@/UserProfile.vue'),
     },
 
 ]
