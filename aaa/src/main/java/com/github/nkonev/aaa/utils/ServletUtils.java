3723b3304ce09d0e71e895caf35aae53c2580e94
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-16 00:55:18 +0400

    Cleanup utils

--- aaa/src/main/java/com/github/nkonev/aaa/utils/ServletUtils.java
+++ aaa/src/main/java/com/github/nkonev/aaa/utils/ServletUtils.java
@@ -13,32 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ServletUtils {
-    public static String nullToEmpty(String s){
-        if (s == null) {
-            return "";
-        } else {
-            return s;
-        }
-    }
-
-    public static String getQuery(HttpServletRequest request) {
-        if (request.getQueryString() == null) {
-            return "";
-        } else {
-            return "?" + request.getQueryString();
-        }
-    }
-
-    public static String getPath(HttpServletRequest request) {
-        String s = request.getRequestURI();
-        if (s == null){
-            return "";
-        }
-        if (s.endsWith("/")){
-            return s.substring(0, s.length()-1);
-        }
-        return s;
-    }
 
 
     public static HttpServletRequest getCurrentHttpRequest(){
@@ -49,27 +23,6 @@ public class ServletUtils {
         return null;
     }
 
-    public static String getIpAddressFromRequestContext() {
-        if (RequestContextHolder.getRequestAttributes() == null) {return "cannot-get-remote-addr";}
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        return getIpAddress(request);
-    }
-
-
-    // http://www.mkyong.com/java/how-to-get-client-ip-address-in-java/
-    // https://stackoverflow.com/a/29910902
-    public static String getIpAddress(HttpServletRequest request) {
-        String remoteAddr = "cannot-get-remote-addr";
-
-        if (request != null) {
-            remoteAddr = request.getHeader("X-FORWARDED-FOR");
-            if (remoteAddr == null || "".equals(remoteAddr)) {
-                remoteAddr = request.getRemoteAddr();
-            }
-        }
-
-        return remoteAddr;
-    }
 
     public static List<String> getAcceptHeaderValues(HttpServletRequest request) {
         final List<String> acceptValues = Collections.list(request.getHeaders(HttpHeaders.ACCEPT))
