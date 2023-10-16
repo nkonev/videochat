54bbe132e7c418c451da81f23949f0d49cb9486a
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-16 15:34:55 +0400

    update livekit and egress, quickfix for egress start-after-stop

---
+++ docker/egress/entrypoint2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Clean out tmp
+rm -rf /home/egress/tmp/*
+
+# Start pulseaudio
+rm -rf /var/run/pulse /var/lib/pulse /home/egress/.config/pulse /home/egress/.cache/xdgr/pulse
+pulseaudio -D --verbose --exit-idle-time=-1 --disallow-exit
+
+# Run egress service
+exec egress
