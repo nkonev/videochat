e3870b007f9a51a2113ac43106b954cc2d9c5861
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-24 19:41:53 +0400

    Improve Welcome.vue

--- frontend/src/Welcome.vue
+++ frontend/src/Welcome.vue
@@ -4,15 +4,19 @@
       <v-card>
         <v-card-title class="d-flex justify-space-around">{{$vuetify.locale.t('$vuetify.welcome_participant', chatStore.currentUser?.login)}}</v-card-title>
         <v-card-actions class="d-flex justify-space-around flex-wrap flex-row pb-0">
+          <v-btn :size="getBtnSize()" @click="findUser()" text :class="isMobile() ? 'my-2' : ''">
+            <v-icon :size="getIconSize()">mdi-account-group</v-icon>
+            {{ $vuetify.locale.t('$vuetify.users') }}
+          </v-btn>
           <v-btn :size="getBtnSize()" color="primary" @click="createChat()" text :class="isMobile() ? 'my-2' : ''">
             <v-icon :size="getIconSize()">mdi-plus</v-icon>
             {{ $vuetify.locale.t('$vuetify.new_chat') }}
           </v-btn>
-          <v-btn :size="getBtnSize()" @click="findUser()" text :class="isMobile() ? 'my-2' : ''">
-            <v-icon :size="getIconSize()">mdi-magnify</v-icon>
-            {{ $vuetify.locale.t('$vuetify.find_user') }}
+          <v-btn :size="getBtnSize()" @click="chats()" text :class="isMobile() ? 'my-2' : ''">
+            <v-icon :size="getIconSize()">mdi-forum</v-icon>
+            {{ $vuetify.locale.t('$vuetify.chats') }}
           </v-btn>
-          <v-btn :size="getBtnSize()" @click="availableChats()" text :class="isMobile() ? 'my-2' : ''">
+          <v-btn :size="getBtnSize()" @click="availableForSearchChats()" text :class="isMobile() ? 'my-2' : ''">
             <v-icon :size="getIconSize()">mdi-forum</v-icon>
             {{ $vuetify.locale.t('$vuetify.public_chats') }}
           </v-btn>
@@ -49,8 +53,11 @@
       findUser() {
         goToPreserving(this.$route, this.$router, { name: profile_list_name});
       },
-      availableChats() {
+      availableForSearchChats() {
         this.$router.push({ name: chat_list_name, hash: null, query: {[SEARCH_MODE_CHATS] : publicallyAvailableForSearchChatsQuery} })
+      },
+      chats() {
+        this.$router.push({ name: chat_list_name })
       },
       goBlog() {
         window.location.href = blog
