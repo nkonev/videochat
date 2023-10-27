19a992a139923dd46db26dd703419d1ba6b7891d
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-27 06:12:19 +0400

    Remove front-old

--- frontend-old/src/blogBus.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-export default new Vue();
-export const SEARCH_STRING_CHANGED = "searchStringChanged";
--- frontend-old/src/blogRoutes.js
+++
@@ -1,2 +0,0 @@
-export const blog = '/blog';
-export const blog_post_name = 'blogPost';
