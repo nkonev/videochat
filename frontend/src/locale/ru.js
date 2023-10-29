b9d0b0f19a2baabf6428b7c5756c8d7523cb87be
Nikita Konev <nikit.cpp@yandex.ru>
2023-10-30 03:18:08 +0400

    Improve translation

--- frontend/src/locale/ru.js
+++ frontend/src/locale/ru.js
@@ -166,7 +166,7 @@ export default {
     copy_link_to_message: "Скопировать ссылку на сообщение",
     copy_link_to_chat: "Скопировать ссылку на чат",
     copy_selected: "Скопировать выделенный текст",
-    pinned_messages: "Закреплённые",
+    pinned_messages: "Закреплённые сообщ.",
     pinned_messages_full: "Закреплённые сообщения",
     pinned_message: "Закреплённое сообщение",
     pin_message: "Закрепить сообщение",
