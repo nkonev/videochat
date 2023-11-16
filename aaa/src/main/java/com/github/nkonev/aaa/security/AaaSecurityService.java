8f95b1ac4423f31588ebb52aef504396e2db6d80
Nikita Konev <nikit.cpp@yandex.ru>
2023-11-16 04:08:37 +0400

    Simplify & optimize AaaSecurityService

--- aaa/src/main/java/com/github/nkonev/aaa/security/AaaSecurityService.java
+++ aaa/src/main/java/com/github/nkonev/aaa/security/AaaSecurityService.java
@@ -11,6 +11,7 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 /**
@@ -23,6 +24,13 @@ public class AaaSecurityService {
 
     @Autowired
     private UserAccountRepository userAccountRepository;
+
+    private UserAccount deleted;
+
+    @PostConstruct
+    public void postConstruct() {
+        deleted = userAccountRepository.findByUsername(Constants.DELETED).orElseThrow();
+    }
 
     public boolean hasSessionManagementPermission(UserAccountDetailsDTO userAccount) {
         if (userAccount==null){
@@ -38,23 +46,10 @@ public class AaaSecurityService {
         if (lockDTO!=null && userAccount.getId().equals(lockDTO.userId())){
             return false;
         }
-        if (roleHierarchy.getReachableGrantedAuthorities(userAccount.getAuthorities()).contains(new SimpleGrantedAuthority(UserRole.ROLE_ADMIN.name()))){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean hasSettingsPermission(UserAccountDetailsDTO userAccount) {
-        return Optional
-                .ofNullable(userAccount)
-                .map(u -> u.getAuthorities()
-                        .contains(new SimpleGrantedAuthority(UserRole.ROLE_ADMIN.name())))
-                .orElse(false);
+        return roleHierarchy.getReachableGrantedAuthorities(userAccount.getAuthorities()).contains(new SimpleGrantedAuthority(UserRole.ROLE_ADMIN.name()));
     }
 
     public boolean canDelete(UserAccountDetailsDTO userAccount, long userIdToDelete) {
-        UserAccount deleted = userAccountRepository.findByUsername(Constants.DELETED).orElseThrow();
         if (deleted.id().equals(userIdToDelete)){
             return false;
         }
@@ -93,9 +88,8 @@ public class AaaSecurityService {
             return false;
         }
         if (currentUser == null) {
-            return  false;
+            return false;
         }
-        UserAccount deleted = userAccountRepository.findByUsername(Constants.DELETED).orElseThrow();
         if (deleted.id().equals(userAccount.id())){
             return false;
         }
