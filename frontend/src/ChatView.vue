c48dd6c7efc12c81c3c5adbc460c86f876e159e2
Nikita Konev <nikit.cpp@yandex.ru>
2023-12-10 06:31:13 +0400

    Fix non-changing green tube button onto red in case user accepts tet-a-tet call by clicking on blinking snackbar

--- frontend/src/ChatView.vue
+++ frontend/src/ChatView.vue
@@ -538,8 +538,10 @@ export default {
       await this.onProfileSet();
     }
 
-    this.chatStore.showCallButton = true;
-    this.chatStore.showHangButton = false;
+    if (this.$route.name == chat_name) { // not to change in case we enter to call by clicking on blinking snackbar
+      this.chatStore.showCallButton = true;
+      this.chatStore.showHangButton = false;
+    }
 
     bus.on(PROFILE_SET, this.onProfileSet);
     bus.on(LOGGED_OUT, this.onLogout);
@@ -587,8 +589,12 @@ export default {
     setTitle(null);
     this.chatStore.avatar = null;
     this.chatStore.showGoToBlogButton = null;
-    this.chatStore.showCallButton = false;
-    this.chatStore.showHangButton = false;
+
+    if (this.$route.name == chat_name) { // not to change in case we enter to call by clicking on blinking snackbar
+      this.chatStore.showCallButton = false;
+      this.chatStore.showHangButton = false;
+    }
+
     this.chatStore.isShowSearch = false;
     this.chatStore.chatUsersCount = 0;
 
