ca8fde9475aa82d15938cd5f7057cf724cde3c07
Nikita Konev <nikit.cpp@yandex.ru>
2024-01-10 07:00:27 +0400

    disable some graphical effects on mobile in order to speedup the app and improve the user experience

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -13,9 +13,9 @@ import { createVuetify } from 'vuetify';
 import {getStoredLanguage} from "@/store/localStore";
 import en from "@/locale/en";
 import ru from "@/locale/ru";
+import {isMobileBrowser} from "@/utils";
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-export default createVuetify({
+const config = {
   theme: {
     themes: {
       light: {
@@ -30,4 +30,17 @@ export default createVuetify({
     locale: getStoredLanguage(),
     messages: { en, ru },
   }
-})
+}
+
+if (isMobileBrowser()) {
+  config.defaults = {
+    global: {
+      transition: false,
+      ripple: false,
+      scrim: false,
+    },
+  }
+}
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify(config);
