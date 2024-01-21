2deceae1048e80389c2486237d02593dd02078a6
Nikita Konev <nikit.cpp@yandex.ru>
2024-01-21 06:11:22 +0400

    sp

--- should-run.sh
+++ should-run.sh
@@ -23,27 +23,32 @@ else
 
   changed_dirs=()
 
+  echo
   for service in "${all_services[@]}"; do
-    echo "Examining service ${service}"
-    if [[ "$service" == "frontend" ]]; then
-      prev_deployed_commit=$(curl -Ss "$website_prefix/git.json" | jq -r '.commit')
-    elif [[ "$service" == "e2e-test" ]]; then
-      prev_deployed_commit=HEAD~1
-    else
-      prev_deployed_commit=$(curl -Ss "$website_prefix/${service}/git.json" | jq -r '.commit')
+
+    pattern_test=$(echo $service | grep -E -q "${service_pattern}" && echo true || echo false)
+    if [[ "$pattern_test" == true ]]; then
+
+      echo "Examining service ${service}"
+      if [[ "$service" == "frontend" ]]; then
+        prev_deployed_commit=$(curl -Ss "$website_prefix/git.json" | jq -r '.commit')
+      elif [[ "$service" == "e2e-test" ]]; then
+        prev_deployed_commit=HEAD~1
+      else
+        prev_deployed_commit=$(curl -Ss "$website_prefix/${service}/git.json" | jq -r '.commit')
+      fi
+
+      echo "Getting change between ${prev_deployed_commit} and ${trigger_commit}"
+      local_changed_dirs=( $(git diff --dirstat=files,0 ${prev_deployed_commit} ${trigger_commit} | sed 's/^[ 0-9.]\+% //g' | cut -d'/' -f1 | uniq) )
+      echo "Since prev deployed commit ${prev_deployed_commit} there are following changed dirs"
+      for changed_dir in "${local_changed_dirs[@]}"; do
+          if [[ "$changed_dir" == "$service" ]]; then
+            echo "-> ${changed_dir}"
+            changed_dirs+=(${changed_dir})
+          fi
+      done
+      echo
     fi
-
-    echo "Getting change between ${prev_deployed_commit} and ${trigger_commit}"
-    local_changed_dirs=( $(git diff --dirstat=files,0 ${prev_deployed_commit} ${trigger_commit} | sed 's/^[ 0-9.]\+% //g' | cut -d'/' -f1 | uniq) )
-    echo "Since prev deployed commit ${prev_deployed_commit} there are following changed dirs"
-    for changed_dir in "${local_changed_dirs[@]}"; do
-        if [[ "$changed_dir" == "$service" ]]; then
-          echo "-> ${changed_dir}"
-          changed_dirs+=(${changed_dir})
-        fi
-    done
-
-    echo
   done
 
   services_list=($(printf "%s\n" "${changed_dirs[@]}" | sort -u))
