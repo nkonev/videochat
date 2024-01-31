0cf9af9d33de035d01261e405034c6290235f526
Nikita Konev <nikit.cpp@yandex.ru>
2024-01-31 19:07:17 +0400

    Fix reaction styles

--- frontend/src/MessageItem.vue
+++ frontend/src/MessageItem.vue
@@ -35,8 +35,8 @@
                     <div :class="embedClass()" v-html="item.embedMessage.text"></div>
                 </div>
                 <v-container v-if="shouldShowMainContainer(item)" v-html="item.text" :class="messageClass(item)"></v-container>
-                <div class="mt-0 mr-2 reactions" v-if="shouldShowReactions(item)">
-                  <v-btn v-for="reaction in item.reactions" variant="flat" size="small" height="32px" rounded class="ml-2 mb-2" @click="onExistingReactionClick(reaction.reaction)"><span v-if="reaction.count > 1" class="text-body-2 with-space">{{ '' + reaction.count + ' ' }}</span><span class="text-h6">{{ reaction.reaction }}</span></v-btn>
+                <div class="mt-0 mr-4 reactions" v-if="shouldShowReactions(item)">
+                  <v-btn v-for="(reaction, i) in item.reactions" variant="flat" size="small" height="32px" rounded :class="reactionClass(i)" @click="onExistingReactionClick(reaction.reaction)"><span v-if="reaction.count > 1" class="text-body-2 with-space">{{ '' + reaction.count + ' ' }}</span><span class="text-h6">{{ reaction.reaction }}</span></v-btn>
                 </div>
             </div>
         </div>
@@ -197,6 +197,14 @@
             },
             onExistingReactionClick(reaction) {
               this.$emit('onreactionclick', {id: this.item.id, reaction: reaction})
+            },
+            reactionClass(i) {
+              const classes = [];
+              classes.push("mb-2")
+              if (i > 0) {
+                classes.push("ml-2")
+              }
+              return classes
             },
         },
         created() {
