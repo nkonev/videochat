nkonev/videochat

commit 680dd24801a37e2a9aaf835ca15b47bc8f9e10ac

Cleanup aaa

aaa/src/main/java/com/github/nkonev/aaa/security/OAuth2ClientRegistrationRepositoryConfig.java
@@ -1,29 +1,0 @@
-package com.github.nkonev.aaa.security;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@EnableConfigurationProperties(OAuth2ClientProperties.class)
-@Configuration
-public class OAuth2ClientRegistrationRepositoryConfig {
-    // Copy-paste from OAuth2ClientRegistrationRepositoryConfiguration
-    @Bean
-    ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
-        List<ClientRegistration> registrations = new ArrayList<>(OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties).values());
-        if (registrations.isEmpty()) {
-            return registrationId -> null;
-        } else {
-            return new InMemoryClientRegistrationRepository(registrations);
-        }
-    }
-
-}

aaa/src/main/java/com/github/nkonev/aaa/security/AaaOAuth2AuthorizationCodeUserService.java
@@ -2,11 +2,9 @@ package com.github.nkonev.aaa.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component

aaa/src/main/java/com/github/nkonev/aaa/security/FacebookOAuth2UserService.java
@@ -9,9 +9,6 @@ import com.github.nkonev.aaa.security.checks.AaaPreAuthenticationChecks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -21,14 +18,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 
 @Transactional
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Component
 public class FacebookOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 

aaa/src/main/java/com/github/nkonev/aaa/security/GoogleOAuth2UserService.java
@@ -9,9 +9,6 @@ import com.github.nkonev.aaa.security.checks.AaaPreAuthenticationChecks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -28,7 +25,6 @@ import java.util.Set;
 
 
 @Transactional
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Component
 public class GoogleOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
 

aaa/src/main/java/com/github/nkonev/aaa/security/KeycloakOAuth2UserService.java
@@ -8,13 +8,9 @@ import com.github.nkonev.aaa.security.checks.AaaPostAuthenticationChecks;
 import com.github.nkonev.aaa.security.checks.AaaPreAuthenticationChecks;
 import com.nimbusds.jose.shaded.json.JSONArray;
 import com.nimbusds.jwt.JWTParser;
-import com.nimbusds.jwt.SignedJWT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -30,7 +26,6 @@ import java.util.stream.Collectors;
 
 
 @Transactional
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Component
 public class KeycloakOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 

aaa/src/main/java/com/github/nkonev/aaa/security/LdapAuthenticationProvider.java
@@ -8,11 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.ldap.NamingException;
 import org.springframework.ldap.core.LdapOperations;
-import org.springframework.ldap.query.LdapQuery;
 import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;

aaa/src/main/java/com/github/nkonev/aaa/security/RESTAuthenticationLogoutSuccessHandler.java
@@ -1,7 +1,6 @@
 package com.github.nkonev.aaa.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.nkonev.aaa.controllers.UserProfileController;
 import com.github.nkonev.aaa.dto.UserAccountDetailsDTO;
 import com.github.nkonev.aaa.services.EventService;
 import org.slf4j.Logger;

aaa/src/main/java/com/github/nkonev/aaa/security/VkontakteOAuth2UserService.java
@@ -10,9 +10,6 @@ import com.github.nkonev.aaa.security.checks.AaaPreAuthenticationChecks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -26,7 +23,6 @@ import java.util.*;
 
 @Component
 @Transactional
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class VkontakteOAuth2UserService extends AbstractOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     @Autowired

commit da5dadd5838643e81916661ba4a609cb2f5a2b8e

Restore (broken in 680dd248) possilility to run aaa without any OAuth 2.0 configured

aaa/src/main/java/com/github/nkonev/aaa/security/OAuth2ClientRegistrationRepositoryConfig.java
@@ -1,0 +1,29 @@
+package com.github.nkonev.aaa.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableConfigurationProperties(OAuth2ClientProperties.class)
+@Configuration
+public class OAuth2ClientRegistrationRepositoryConfig {
+    // Copy-paste from OAuth2ClientRegistrationRepositoryConfiguration
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
+        List<ClientRegistration> registrations = new ArrayList<>(OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties).values());
+        if (registrations.isEmpty()) {
+            return registrationId -> null;
+        } else {
+            return new InMemoryClientRegistrationRepository(registrations);
+        }
+    }
+
+}