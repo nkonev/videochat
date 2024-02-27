d9fc97ae1012535a73fa3f90cf5035f8c6676e7a
Nikita Konev <nikit.cpp@yandex.ru>
2024-02-27 14:51:19 +0400

    migrate aaa scripts to make

--- aaa/run-oauth2-emu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec ./mvnw -Poauth2_emulator spring-boot:run
--- aaa/run-with-oauth2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# You need to start EmulatorServersController firstly
-# export JAVA_HOME=/usr/lib/jvm/bellsoft-java17.x86_64
-./mvnw clean package -DskipTests
-exec $JAVA_HOME/bin/java -jar target/aaa-0.0.0-exec.jar --spring.config.location=file:src/main/resources/config/application.yml,file:src/test/resources/config/oauth2-basic.yml,file:src/test/resources/config/user-test-controller.yml,file:src/test/resources/config/demo-migration.yml,file:src/test/resources/config/log-email.yml
