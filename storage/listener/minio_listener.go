bd7a9d3e8b87155c5eac132c74de98cf8e10e198
Nikita Konev <nikit.cpp@yandex.ru>
2024-03-15 00:57:24 +0400

    fix wrong chatId for deleting a file

--- storage/listener/minio_listener.go
+++ storage/listener/minio_listener.go
@@ -22,7 +22,7 @@ func CreateMinioEventsListener(previewService *services.PreviewService, eventSer
 		eventName := gjson.Get(strData, "EventName").String()
 		key := gjson.Get(strData, "Key").String()
 		userMetadata := gjson.Get(strData, "Records.0.s3.object.userMetadata") // empty in case delete
-		chatId := userMetadata.Get(services.ChatIdKey(true)).Int()
+		maybeChatId := userMetadata.Get(services.ChatIdKey(true)).Int()
 		ownerId := userMetadata.Get(services.OwnerIdKey(true)).Int()
 		correlationIdStr := userMetadata.Get(services.CorrelationIdKey(true)).String()
 		isRecording := userMetadata.Get(services.RecordingKey(true)).Bool()
@@ -33,7 +33,7 @@ func CreateMinioEventsListener(previewService *services.PreviewService, eventSer
 		var minioEvent = &dto.MinioEvent{
 			EventName:     eventName,
 			Key:           key,
-			ChatId:        chatId,
+			ChatId:        maybeChatId,
 			OwnerId:       ownerId,
 			CorrelationId: correlationId,
 		}
@@ -63,7 +63,7 @@ func CreateMinioEventsListener(previewService *services.PreviewService, eventSer
 
 		err = client.GetChatParticipantIds(workingChatId, ctx, func(participantIds []int64) error {
 			if errEventService == nil && isEventForEventService(eventType) {
-				eventService.SendToParticipants(normalizedKey, chatId, eventType, participantIds, eventServiceResponse, ctx)
+				eventService.SendToParticipants(normalizedKey, workingChatId, eventType, participantIds, eventServiceResponse, ctx)
 			}
 			if isEventForPreviewService(eventType) {
 				previewService.SendToParticipants(minioEvent, participantIds, previewServiceResponse, ctx)
