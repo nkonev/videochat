d2c89d770401d78846ae59310b2a870a5068292f
Nikita Konev <nikit.cpp@yandex.ru>
2024-03-21 23:59:23 +0400

    Initial Vike

---
+++ blog/renderer/+config.js
@@ -0,0 +1,17 @@
+// https://vike.dev/config
+export default {
+  // https://vike.dev/clientRouting
+  clientRouting: true,
+  // https://vike.dev/meta
+  meta: {
+    // Define new setting 'title'
+    title: {
+      env: { server: true, client: true }
+    },
+    // Define new setting 'description'
+    description: {
+      env: { server: true }
+    }
+  },
+  hydrationCanBeAborted: true
+}
