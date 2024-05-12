a1e23ed4dc6062aa2769f6e6f959c597e15297a4
Nikita Konev <nikit.cpp@yandex.ru>
2024-05-12 20:07:45 +0400

    Update AAA to Spring Boot 3.2.5, eliminate Tomcat's leak detector warning due to BatchSpanProcessor

--- aaa/src/main/java/name/nkonev/aaa/config/TracingConfig.java
+++ aaa/src/main/java/name/nkonev/aaa/config/TracingConfig.java
@@ -6,15 +6,22 @@ import io.micrometer.tracing.otel.propagation.BaggageTextMapPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.extension.trace.propagation.JaegerPropagator;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class TracingConfig {
+
+    @Autowired
+    private BatchSpanProcessor batchSpanProcessor;
 
     @Bean
     TextMapPropagator jaegerTextMapPropagatorWithBaggage(TracingProperties tracingProperties, OtelCurrentTraceContext otelCurrentTraceContext) {
@@ -29,6 +36,13 @@ public class TracingConfig {
     @Bean
     ContextPropagators otelContextPropagators(TextMapPropagator jaegerTextMapPropagatorWithBaggage) {
         return ContextPropagators.create(jaegerTextMapPropagatorWithBaggage);
+    }
+
+    @PreDestroy
+    public void pd() {
+        batchSpanProcessor.forceFlush().join(1, TimeUnit.MILLISECONDS).succeed().whenComplete(() -> {
+                batchSpanProcessor.shutdown().join(1, TimeUnit.MILLISECONDS);
+        }).succeed();
     }
 
 }
