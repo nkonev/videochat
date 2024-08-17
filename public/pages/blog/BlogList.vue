0e94e6ab666a1a8408215ddcb6883d056511ae76
Nikita Konev <nikit.cpp@yandex.ru>
2024-08-17 04:06:34 +0300

    Add posts not found

--- public/pages/blog/BlogList.vue
+++ public/pages/blog/BlogList.vue
@@ -1,60 +1,65 @@
 <template>
   <v-container class="ma-0 pa-0 my-list-container" fluid>
-    <div class="my-blog-scroller" id="blog-post-list">
+    <template v-if="items.length">
+        <div class="my-blog-scroller" id="blog-post-list">
 
-      <v-card
-        v-for="(item, index) in items"
-        :key="item.id"
-        :id="getItemId(item.id)"
-        class="mb-2 mr-2 blog-item-root"
-        :min-width="isMobile() ? 200 : 400"
-        max-width="600"
-      >
-        <v-card-text class="pb-0">
-          <v-card>
-            <v-img
-              class="text-white align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              cover
-              :height="isMobile() ? 200 : 300"
-              :src="item.imageUrl"
-            >
-              <v-container class="post-title ma-0 pa-0">
-                <v-card-title>
-                  <a class="post-title-text" v-html="item.title" :href="getLink(item)"></a>
-                </v-card-title>
-              </v-container>
-            </v-img>
+          <v-card
+            v-for="(item, index) in items"
+            :key="item.id"
+            :id="getItemId(item.id)"
+            class="mb-2 mr-2 blog-item-root"
+            :min-width="isMobile() ? 200 : 400"
+            max-width="600"
+          >
+            <v-card-text class="pb-0">
+              <v-card>
+                <v-img
+                  class="text-white align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  cover
+                  :height="isMobile() ? 200 : 300"
+                  :src="item.imageUrl"
+                >
+                  <v-container class="post-title ma-0 pa-0">
+                    <v-card-title>
+                      <a class="post-title-text" v-html="item.title" :href="getLink(item)"></a>
+                    </v-card-title>
+                  </v-container>
+                </v-img>
+              </v-card>
+            </v-card-text>
+
+            <v-card-text class="post-text pb-0" v-html="item.preview">
+            </v-card-text>
+
+            <v-card-actions v-if="item?.owner != null">
+              <v-list-item class="px-0 ml-2">
+                  <template v-slot:prepend v-if="hasLength(item?.owner?.avatar)">
+                      <div class="item-avatar mr-3">
+                          <a :href="getProfileLink(item.owner)" class="user-link">
+                              <img :src="item?.owner?.avatar">
+                          </a>
+                      </div>
+                  </template>
+
+                  <template v-slot:default>
+                      <v-list-item-title><a :href="getProfileLink(item.owner)" class="nodecorated-link" :style="getLoginColoredStyle(item.owner, true)">{{ item?.owner?.login }}</a></v-list-item-title>
+                      <v-list-item-subtitle>
+                          {{ getDate(item) }}
+                      </v-list-item-subtitle>
+
+                  </template>
+
+              </v-list-item>
+            </v-card-actions>
           </v-card>
-        </v-card-text>
 
-        <v-card-text class="post-text pb-0" v-html="item.preview">
-        </v-card-text>
-
-        <v-card-actions v-if="item?.owner != null">
-          <v-list-item class="px-0 ml-2">
-              <template v-slot:prepend v-if="hasLength(item?.owner?.avatar)">
-                  <div class="item-avatar mr-3">
-                      <a :href="getProfileLink(item.owner)" class="user-link">
-                          <img :src="item?.owner?.avatar">
-                      </a>
-                  </div>
-              </template>
-
-              <template v-slot:default>
-                  <v-list-item-title><a :href="getProfileLink(item.owner)" class="nodecorated-link" :style="getLoginColoredStyle(item.owner, true)">{{ item?.owner?.login }}</a></v-list-item-title>
-                  <v-list-item-subtitle>
-                      {{ getDate(item) }}
-                  </v-list-item-subtitle>
-
-              </template>
-
-          </v-list-item>
-        </v-card-actions>
-      </v-card>
-
-      <v-divider/>
-      <v-pagination v-model="page" @update:modelValue="onClickPage" :length="pagesCount" v-if="shouldShowPagination()" :total-visible="pagesCount < 10 && !isMobile() ? 10 : undefined"/>
+          <v-divider/>
+          <v-pagination v-model="page" @update:modelValue="onClickPage" :length="pagesCount" v-if="shouldShowPagination()" :total-visible="pagesCount < 10 && !isMobile() ? 10 : undefined"/>
+        </div>
+    </template>
+    <div v-else>
+      <h1>Posts not found</h1>
     </div>
 
   </v-container>
