8b2e43b9672eb304df9fb3b851d3e42c07e45931
Nikita Konev <nikit.cpp@yandex.ru>
2024-09-17 04:29:03 +0300

    Add title for video buttons

--- frontend/src/TipTapVideo.js
+++ frontend/src/TipTapVideo.js
@@ -42,8 +42,8 @@ const Video = Node.create({
         return [
             'span', {"class": "video-in-message-wrapper"},
             ['img', mergeAttributes({"class": "video-custom-class", "controls": true}, HTMLAttributes)],
-            ['span', {"class": "video-in-message-button mdi mdi-fullscreen"}],
-            ['span', {"class": "video-in-message-button-replace mdi mdi-play-box-outline"}],
+            ['span', {"class": "video-in-message-button mdi mdi-fullscreen", "title": "Open in player"}],
+            ['span', {"class": "video-in-message-button-replace mdi mdi-play-box-outline", "title": "Play in-place"}],
         ];
     },
     addCommands() {
