e557a948cf1edf1b00d2da4b249dc7ca1cd5409e
Nikita Konev <nikit.cpp@yandex.ru>
2024-10-11 01:40:57 +0300

    copy-paste recipe

--- frontend/src/graphql/graphql.js
+++ frontend/src/graphql/graphql.js
@@ -2,14 +2,76 @@ import { createClient } from 'graphql-ws';
 import {getWebsocketUrlPrefix} from "@/utils";
 import bus, {LOGGED_OUT, WEBSOCKET_CONNECTED, WEBSOCKET_LOST, WEBSOCKET_RESTORED} from "@/bus/bus";
 
+function createRestartableClient(options) {
+    let restartRequested = false;
+    let restart = () => {
+        restartRequested = true;
+    };
+
+    const client = createClient({
+        ...options,
+        on: {
+            ...options.on,
+            opened: (socket) => {
+                options.on?.opened?.(socket);
+
+                restart = () => {
+                    if (socket.readyState === WebSocket.OPEN) {
+                        // if the socket is still open for the restart, do the restart
+                        socket.close(4205, 'Client Restart');
+                    } else {
+                        // otherwise the socket might've closed, indicate that you want
+                        // a restart on the next opened event
+                        restartRequested = true;
+                    }
+                };
+
+                // just in case you were eager to restart
+                if (restartRequested) {
+                    restartRequested = false;
+                    restart();
+                }
+            },
+        },
+    });
+
+    return {
+        ...client,
+        restart: () => restart(),
+    };
+}
+
+let timedOut;
 let graphQlClient;
 export const createGraphQlClient = () => {
     let initialized = false;
 
-    // https://github.com/enisdenjo/graphql-ws#use-the-client
-    graphQlClient = createClient({
+    // url: getWebsocketUrlPrefix() + '/api/event/graphql',
+    graphQlClient = createRestartableClient({
         url: getWebsocketUrlPrefix() + '/api/event/graphql',
-    });
+        keepAlive: 10_000, // ping server every 10 seconds
+        on: {
+            ping: (received) => {
+                if (!received /* sent */) {
+                    timedOut = setTimeout(() => {
+                        // a close event `4499: Terminated` is issued to the current WebSocket and an
+                        // artificial `{ code: 4499, reason: 'Terminated', wasClean: false }` close-event-like
+                        // object is immediately emitted without waiting for the one coming from `WebSocket.onclose`
+                        //
+                        // calling terminate is not considered fatal and a connection retry will occur as expected
+                        //
+                        // see: https://github.com/enisdenjo/graphql-ws/discussions/290
+                        graphQlClient.terminate();
+                    }, 5_000);
+                }
+            },
+            pong: (received) => {
+                if (received) {
+                    clearTimeout(timedOut);
+                }
+            },
+        },
+    })
 
     graphQlClient.on('connected', () => {
         if (initialized) {
@@ -28,7 +90,10 @@ export const createGraphQlClient = () => {
         console.info("Close GraphQL", ev);
         bus.emit(WEBSOCKET_LOST);
     });
-    bus.on(LOGGED_OUT, () => {initialized = false});
+    bus.on(LOGGED_OUT, () => {
+        initialized = false;
+        graphQlClient.terminate();
+    });
 }
 
 export const destroyGraphqlClient = () => {
