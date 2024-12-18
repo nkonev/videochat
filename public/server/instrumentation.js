61bfcaa6eb188240675d4c89017e9567a9f622d6
Nikita Konev <nikit.cpp@yandex.ru>
2024-12-18 15:25:10 +0300

    cleanup

--- public/server/instrumentation.js
+++ public/server/instrumentation.js
@@ -1,15 +1,9 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-// const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-// const {
-//     PeriodicExportingMetricReader,
-//     ConsoleMetricExporter,
-// } = require('@opentelemetry/sdk-metrics');
 import { Resource } from '@opentelemetry/resources';
 import {
     ATTR_SERVICE_NAME,
     ATTR_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
-// const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 const collectorOptions = {
@@ -19,24 +13,12 @@ const collectorOptions = {
 };
 
 const exporter = new OTLPTraceExporter(collectorOptions);
-// const provider = new BasicTracerProvider({
-//     spanProcessors: [
-//         new BatchSpanProcessor(exporter, {
-//             // The maximum queue size. After the size is reached spans are dropped.
-//             maxQueueSize: 1000,
-//             // The interval between two consecutive exports
-//             scheduledDelayMillis: 30000,
-//         })
-//     ]
-// });
-// provider.register();
 
 const sdk = new NodeSDK({
     resource: new Resource({
         [ATTR_SERVICE_NAME]: 'public',
         [ATTR_SERVICE_VERSION]: '0.0.0',
     }),
-    // traceExporter: new ConsoleSpanExporter(),
     // metricReader: new PeriodicExportingMetricReader({
     //     exporter: new ConsoleMetricExporter(),
     // }),
