2483c0034ae7823e1dc625d1820eba03f8d6b739
Nikita Konev <nikit.cpp@yandex.ru>
2025-01-23 04:20:49 +0300

    rm short-cirquit

--- frontend/src/MessageList.vue
+++ frontend/src/MessageList.vue
@@ -736,7 +736,6 @@
           return isMessageHash(hash)
         },
         onFocus() {
-          return // TODO rm
           if (this.chatStore.currentUser && this.items && this.isScrolledToBottom()) {
             const bottomNElements = this.items.slice(0, PAGE_SIZE);
             axios.post(`/api/chat/${this.chatId}/message/fresh`, bottomNElements, {
