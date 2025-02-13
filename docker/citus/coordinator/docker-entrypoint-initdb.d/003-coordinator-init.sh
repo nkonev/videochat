ca44dcd4bedccfb359d8622485701737ff96ef47
Nikita Konev <nikit.cpp@yandex.ru>
2025-02-13 23:26:07 +0300

    improve scripts

--- docker/citus/coordinator/docker-entrypoint-initdb.d/003-coordinator-init.sh
+++ docker/citus/coordinator/docker-entrypoint-initdb.d/003-coordinator-init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 /sbin/wait-for-it.sh -t 30 postgresql-citus-worker-1:5432 -- echo 'postgresql-citus-worker-1 is up'
 /sbin/wait-for-it.sh -t 30 postgresql-citus-worker-2:5432 -- echo 'postgresql-citus-worker-2 is up'
 
-cat /opt/init/make-cluster.sql | psql -U postgres
+cat << EOF | psql -U postgres
+select citus_set_coordinator_host('postgresql-citus-coordinator-1', 5432);
+select * from citus_add_node('postgresql-citus-worker-1', 5432);
+select * from citus_add_node('postgresql-citus-worker-2', 5432);
+EOF
 
 echo "cluster is successfully set up"
--- docker/citus/coordinator/opt/init/make-cluster.sql
+++
@@ -1,3 +0,0 @@
-select citus_set_coordinator_host('postgresql-citus-coordinator-1', 5432);
-select * from citus_add_node('postgresql-citus-worker-1', 5432);
-select * from citus_add_node('postgresql-citus-worker-2', 5432);
