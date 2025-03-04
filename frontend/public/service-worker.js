9e156b27d5e782b5fdada3457562681fbfd5dbd4
Nikita Konev <nikit.cpp@yandex.ru>
2025-03-05 00:43:58 +0300

    fix null navigator.serviceWorker.controller

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,3 +1,13 @@
+// to fix navigator.serviceWorker.controller == null
+// https://stackoverflow.com/questions/38168276/navigator-serviceworker-controller-is-null-until-page-refresh/38690771#38690771
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting()); // Activate worker immediately
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim()); // Become available to all pages
+});
+
 
 // Show notification when received
 self.addEventListener('message', (event) => {
