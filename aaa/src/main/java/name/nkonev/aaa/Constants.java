c34310fdfcaf757e8d1d63094c00d875644c1bfe
Nikita Konev <nikit.cpp@yandex.ru>
2025-09-12 23:04:54 +0300

    increase internal users limitation to fit 20(chats) * 10(displayable users per chat) = 200

--- aaa/src/main/java/name/nkonev/aaa/Constants.java
+++ aaa/src/main/java/name/nkonev/aaa/Constants.java
@@ -84,7 +84,7 @@ public class Constants {
 
     public static final int MIN_PASSWORD_LENGTH = 6;
     public static final int MAX_PASSWORD_LENGTH = 100;
-    public static final int MAX_USERS_RESPONSE_LENGTH = 100;
+    public static final int MAX_USERS_RESPONSE_LENGTH = 1000;
 
     public static final int MAX_SMILEYS_LENGTH = 2048;
 
